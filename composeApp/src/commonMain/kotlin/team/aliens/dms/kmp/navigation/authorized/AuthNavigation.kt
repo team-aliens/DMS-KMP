e49682fda7c9c601f8536fd34bcec68025bdd860
uiel <parkuiery@gmail.com>
2024-09-12 20:14:49 +0900

    feat :: navigation 추가

---
+++ composeApp/src/commonMain/kotlin/team/aliens/dms/kmp/navigation/authorized/AuthNavigation.kt
@@ -0,0 +1,18 @@
+package team.aliens.dms.kmp.navigation.authorized
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.navigation
+import team.aliens.dms.kmp.DmsNavigator
+
+private const val NAVIGATION_AUTH = "auth"
+
+internal fun NavGraphBuilder.authNavigation(
+    navigator: DmsNavigator,
+) {
+    navigation(
+        route = NAVIGATION_AUTH,
+        startDestination = "",
+    ) {
+
+    }
+}
