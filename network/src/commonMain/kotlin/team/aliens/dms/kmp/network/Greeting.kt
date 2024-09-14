5c69232184864b03892aa1f1003a66f0c544e06a
uiel <parkuiery@gmail.com>
2024-09-14 23:18:47 +0900

    delete :: 파일 삭제

--- network/src/commonMain/kotlin/team/aliens/dms/kmp/network/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package team.aliens.dms.kmp.network
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- network/src/commonMain/kotlin/team/aliens/dms/kmp/network/Platform.kt
+++
@@ -1,7 +0,0 @@
-package team.aliens.dms.kmp.network
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
