91ef9cc346282bb5e6b3733c5e4e67d7a1264459
uiel <parkuiery@gmail.com>
2024-09-15 20:57:26 +0900

    refactor :: preview 삭제

--- composeApp/src/androidMain/kotlin/team/aliens/dms/kmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/team/aliens/dms/kmp/MainActivity.kt
@@ -3,8 +3,6 @@ package team.aliens.dms.kmp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +12,4 @@ class MainActivity : ComponentActivity() {
             DmsApp()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    DmsApp()
 }
