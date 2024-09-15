dfe58d343d0eb40cfe6506960e695a750d3fc8cd
uiel <parkuiery@gmail.com>
2024-09-15 20:59:21 +0900

    refactor :: koin 초기화 추가

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -2,6 +2,11 @@ import SwiftUI
 
 @main
 struct iOSApp: App {
+    
+    init() {
+        KoinKt.doInitKoin()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
