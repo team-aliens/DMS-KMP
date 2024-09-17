4a85f590aed79b4497be971114d247d5b1df142b
uiel <parkuiery@gmail.com>
2024-09-17 16:36:48 +0900

    feat :: DataStore 구현

---
+++ core/datastore/src/iosMain/kotlin/team/aliens/dms/kmp/core/datastore/IosDataStore.kt
@@ -0,0 +1,49 @@
+package team.aliens.dms.kmp.core.datastore
+
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSURL
+import platform.Foundation.NSUserDomainMask
+
+@OptIn(ExperimentalForeignApi::class)
+internal class IosDataStore {
+    fun getJwtDataStore(): PreferencesDataStore = getDataStore(
+        producePath = {
+            val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
+                directory = NSDocumentDirectory,
+                inDomain = NSUserDomainMask,
+                appropriateForURL = null,
+                create = false,
+                error = null,
+            )
+            requireNotNull(documentDirectory).path() + "/$JWT_STORE"
+        }
+    )
+
+    fun getFeaturesDataStore(): PreferencesDataStore = getDataStore(
+        producePath = {
+            val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
+                directory = NSDocumentDirectory,
+                inDomain = NSUserDomainMask,
+                appropriateForURL = null,
+                create = false,
+                error = null,
+            )
+            requireNotNull(documentDirectory).path() + "/$FEATURES_STORE"
+        }
+    )
+
+    fun getDeviceDataStore(): PreferencesDataStore = getDataStore(
+        producePath = {
+            val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
+                directory = NSDocumentDirectory,
+                inDomain = NSUserDomainMask,
+                appropriateForURL = null,
+                create = false,
+                error = null,
+            )
+            requireNotNull(documentDirectory).path() + "/$DEVICE_STORE"
+        }
+    )
+}
