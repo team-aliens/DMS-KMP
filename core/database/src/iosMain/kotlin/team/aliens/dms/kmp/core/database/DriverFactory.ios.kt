49f3d2d23fc15387daf0a2b0b43e37611291edaf
uiel <parkuiery@gmail.com>
2024-09-20 16:06:35 +0900

    feat :: DeiverFactory 구현

---
+++ core/database/src/iosMain/kotlin/team/aliens/dms/kmp/core/database/DriverFactory.ios.kt
@@ -0,0 +1,10 @@
+package team.aliens.dms.kmp.core.database
+
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.native.NativeSqliteDriver
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual class DriverFactory {
+    actual fun createDriver(): SqlDriver =
+        NativeSqliteDriver(DmsDatabase.Schema,"${DatabaseConstants.NAME}.db")
+}
