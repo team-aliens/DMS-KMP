3843bacef5b24161e1e995245a8a69026473bcd6
uiel <parkuiery@gmail.com>
2024-09-21 10:07:09 +0900

    feat :: DatabaseMealDataSource 구현

---
+++ database/src/commonMain/kotlin/team/aliens/dms/kmp/database/meal/DatabaseMealDataSource.kt
@@ -0,0 +1,10 @@
+package team.aliens.dms.kmp.database.meal
+
+import kotlinx.datetime.LocalDate
+import team.aliens.dms.kmp.core.database.MealEntity
+
+abstract class DatabaseMealDataSource {
+    abstract suspend fun queryMeal(date: LocalDate): MealEntity
+    abstract suspend fun saveMeal(meal: MealEntity)
+    abstract suspend fun saveAllMeals(meals: List<MealEntity>)
+}
