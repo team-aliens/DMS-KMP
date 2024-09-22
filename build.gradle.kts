ec998eed7daa969a48d031f08ae8ac696638c395
uiel <parkuiery@gmail.com>
2024-09-22 10:25:50 +0900

    build :: 파이어베이스 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.googleServices) apply false
 }
