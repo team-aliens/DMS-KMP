a9d71f1e7018b3825412d8474c2f921d5ed6de2d
uiel <parkuiery@gmail.com>
2024-09-22 10:26:40 +0900

    feat :: iOS 파이어베이스 세팅

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -5,6 +5,8 @@ struct iOSApp: App {
     
     init() {
         KoinKt.doInitKoin()
+        FirebaseApp.configure()
+        FirebaseApp.crashlytics.setCrashlyticsCollectionEnabled(true)
     }
     
     var body: some Scene {
