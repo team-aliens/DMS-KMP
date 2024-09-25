ed78004e4422d1ed832f90b5a40bbb6205d9c67d
uiel <parkuiery@gmail.com>
2024-09-25 21:18:46 +0900

    refactor :: Instant 타입으로 변경

--- core/datastore/src/commonMain/kotlin/team/aliens/dms/kmp/core/datastore/jwt/model/Tokens.kt
+++ core/datastore/src/commonMain/kotlin/team/aliens/dms/kmp/core/datastore/jwt/model/Tokens.kt
@@ -1,6 +1,6 @@
 package team.aliens.dms.kmp.core.datastore.jwt.model
 
-import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.Instant
 import team.aliens.dms.kmp.network.auth.model.TokensResponse
 
 data class Tokens(
@@ -10,26 +10,26 @@ data class Tokens(
 
 sealed class Token {
     abstract val value: String
-    abstract val expiration: LocalDateTime
+    abstract val expiration: Instant
 }
 
 data class AccessToken(
     override val value: String,
-    override val expiration: LocalDateTime,
+    override val expiration: Instant,
 ) : Token()
 
 data class RefreshToken(
     override val value: String,
-    override val expiration: LocalDateTime,
+    override val expiration: Instant,
 ) : Token()
 
 internal fun TokensResponse.toModel(): Tokens = Tokens(
     accessToken = AccessToken(
         value = this.accessToken,
-        expiration = LocalDateTime.parse(this.accessTokenExpiration),
+        expiration = this.accessTokenExpiration,
     ),
     refreshToken = RefreshToken(
         value = this.refreshToken,
-        expiration = LocalDateTime.parse(this.refreshTokenExpiration),
+        expiration = this.refreshTokenExpiration,
     ),
 )
--- network/src/commonMain/kotlin/team/aliens/dms/kmp/network/auth/model/TokensResponse.kt
+++ network/src/commonMain/kotlin/team/aliens/dms/kmp/network/auth/model/TokensResponse.kt
@@ -1,12 +1,13 @@
 package team.aliens.dms.kmp.network.auth.model
 
+import kotlinx.datetime.Instant
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class TokensResponse(
     @SerialName("access_token") val accessToken: String,
-    @SerialName("access_token_expired_at") val accessTokenExpiration: String,
+    @SerialName("access_token_expired_at") val accessTokenExpiration: Instant,
     @SerialName("refresh_token") val refreshToken: String,
-    @SerialName("refresh_token_expired_at") val refreshTokenExpiration: String,
+    @SerialName("refresh_token_expired_at") val refreshTokenExpiration: Instant,
 )
