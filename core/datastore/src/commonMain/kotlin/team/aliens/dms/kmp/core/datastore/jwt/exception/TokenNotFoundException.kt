565dc0719429ec93a0d3e8ce404b7985b358b654
uiel <parkuiery@gmail.com>
2024-09-25 09:46:07 +0900

    feat :: TokenNotFoundException 구현

---
+++ core/datastore/src/commonMain/kotlin/team/aliens/dms/kmp/core/datastore/jwt/exception/TokenNotFoundException.kt
@@ -0,0 +1,9 @@
+package team.aliens.dms.kmp.core.datastore.jwt.exception
+
+import team.aliens.dms.kmp.core.datastore.exception.LoadFailureException
+
+sealed class TokenNotFoundException(message: String?) : LoadFailureException(message)
+
+class AccessTokenNotFoundException : TokenNotFoundException("Access token not found")
+
+class RefreshTokenNotFoundException : TokenNotFoundException("Refresh token not found")
