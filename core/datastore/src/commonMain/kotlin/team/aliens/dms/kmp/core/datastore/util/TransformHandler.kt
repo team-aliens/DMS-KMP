ecb4ac6eb895249fc46557b82777cb2b6a32d412
uiel <parkuiery@gmail.com>
2024-09-25 23:34:43 +0900

    refactor :: 접근제한자 변경

--- core/datastore/src/commonMain/kotlin/team/aliens/dms/kmp/core/datastore/util/TransformHandler.kt
+++ core/datastore/src/commonMain/kotlin/team/aliens/dms/kmp/core/datastore/util/TransformHandler.kt
@@ -2,7 +2,7 @@ package team.aliens.dms.kmp.core.datastore.util
 
 import team.aliens.dms.kmp.core.datastore.exception.TransformFailureException
 
-internal suspend inline fun <reified T> transform(
+suspend inline fun <reified T> transform(
     onSuccess: (T) -> Unit = {},
     onFailure: (Throwable) -> Unit = { throw TransformFailureException() },
     crossinline block: suspend () -> T,
