fe2621f44a8f8daffc80446165d3f83d6c096f21
uiel <parkuiery@gmail.com>
2024-09-25 23:34:30 +0900

    refactor :: jwt 의존성 변경

--- core/datastore/src/commonMain/kotlin/team/aliens/dms/kmp/core/datastore/di/DataStoreModule.kt
+++ core/datastore/src/commonMain/kotlin/team/aliens/dms/kmp/core/datastore/di/DataStoreModule.kt
@@ -2,13 +2,9 @@ package team.aliens.dms.kmp.core.datastore.di
 
 import org.koin.core.module.Module
 import org.koin.dsl.module
-import team.aliens.dms.kmp.core.datastore.jwt.JwtDataSource
-import team.aliens.dms.kmp.core.datastore.jwt.JwtDataSourceImpl
 
 val dataStoreModule =
     module {
-        single<JwtDataSource> { JwtDataSourceImpl(get()) }
-
         includes(platformDataStoreModule)
     }
 
