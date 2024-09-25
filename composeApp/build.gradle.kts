5e1997671c2f04951e3e71f27c4b8cb358fb26d4
uiel <parkuiery@gmail.com>
2024-09-25 23:29:47 +0900

    chore :: jwt 의존성 추가

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -57,6 +57,7 @@ kotlin {
             implementation(projects.core.network)
             implementation(projects.core.datastore)
             implementation(projects.core.database)
+            implementation(projects.core.jwt)
             implementation(projects.database)
         }
         jvmMain.dependencies {
--- composeApp/src/commonMain/kotlin/team/aliens/dms/kmp/di/Koin.kt
+++ composeApp/src/commonMain/kotlin/team/aliens/dms/kmp/di/Koin.kt
@@ -2,15 +2,16 @@ package team.aliens.dms.kmp.di
 
 import org.koin.core.context.startKoin
 import team.aliens.dms.kmp.core.datastore.di.dataStoreModule
+import team.aliens.dms.kmp.core.jwt.di.jwtModule
 import team.aliens.dms.kmp.core.network.di.networkModule
-import team.aliens.dms.kmp.database.di.dataSourceModule
+import team.aliens.dms.kmp.database.di.databaseModule
 
 fun appModule() =
     listOf(
         networkModule,
         dataStoreModule,
-        dataStoreModule,
-        dataSourceModule,
+        databaseModule,
+        jwtModule,
     )
 
 fun initKoin() {
