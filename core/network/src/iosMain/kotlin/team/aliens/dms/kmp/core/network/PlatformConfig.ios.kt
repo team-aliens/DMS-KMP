541c6434b3b0ea52f8924a0f121779d8ec6545d7
uiel <parkuiery@gmail.com>
2024-09-25 21:23:42 +0900

    refactor :: 예외 처리

--- core/network/src/iosMain/kotlin/team/aliens/dms/kmp/core/network/PlatformConfig.ios.kt
+++ core/network/src/iosMain/kotlin/team/aliens/dms/kmp/core/network/PlatformConfig.ios.kt
@@ -1,8 +1,11 @@
 package team.aliens.dms.kmp.core.network
 
 import platform.Foundation.NSBundle
+import team.aliens.dms.kmp.core.network.exception.CannotFindIosBaseurlException
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual object PlatformConfig {
-    actual val baseUrl: String = NSBundle.mainBundle.objectForInfoDictionaryKey("BaseURL") as String
+    actual val baseUrl: String =
+        (NSBundle.mainBundle.objectForInfoDictionaryKey("BaseURL") as? String)
+            ?: throw CannotFindIosBaseurlException()
 }
