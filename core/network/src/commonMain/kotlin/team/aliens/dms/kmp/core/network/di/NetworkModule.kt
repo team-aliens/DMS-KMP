3a7e7c8acbd7a8738acf80273fd28b2f37e85f17
uiel <parkuiery@gmail.com>
2024-09-27 09:29:22 +0900

    refactor :: ktlint

--- core/network/src/commonMain/kotlin/team/aliens/dms/kmp/core/network/di/NetworkModule.kt
+++ core/network/src/commonMain/kotlin/team/aliens/dms/kmp/core/network/di/NetworkModule.kt
@@ -2,21 +2,13 @@ package team.aliens.dms.kmp.core.network.di
 
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.observer.ResponseObserver
-import io.ktor.client.request.accept
-import io.ktor.client.request.header
-import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
-import io.ktor.http.URLProtocol
-import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.dsl.module
-import team.aliens.dms.kmp.core.network.PlatformConfig
 
 val networkModule =
     module {
