49824b625c64e9b54f4ee5c32e51e7f2d368f083
uiel <parkuiery@gmail.com>
2024-09-30 23:39:45 +0900

    chore :: 의존성 추가

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -47,6 +47,7 @@ kotlin {
             implementation(compose.foundation)
             implementation(compose.material)
             implementation(compose.ui)
+            implementation(compose.material3)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
