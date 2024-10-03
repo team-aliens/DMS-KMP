79c6017d7eef6245f6b4829ccc67dda0704c1a1f
uiel <parkuiery@gmail.com>
2024-10-03 15:29:05 +0900

    feat :: DmsTextField 구현

---
+++ core/design-system/src/commonMain/kotlin/team/aliens/dms/kmp/core/designsystem/textfield/DmsTextField.kt
@@ -0,0 +1,197 @@
+package team.aliens.dms.kmp.core.designsystem.textfield
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import team.aliens.dms.kmp.core.designsystem.foundation.DmsIcon
+import team.aliens.dms.kmp.core.designsystem.foundation.DmsTheme
+import team.aliens.dms.kmp.core.designsystem.foundation.DmsTypography
+import team.aliens.dms.kmp.core.designsystem.text.DmsText
+
+@Composable
+fun DmsTextField(
+    modifier: Modifier = Modifier,
+    label: String? = null,
+    value: String,
+    hint: String,
+    onValueChange: (String) -> Unit,
+    description: String? = null,
+    readOnly: Boolean = false,
+    imeAction: ImeAction = ImeAction.Done,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = true,
+    maxLength: Int = Int.MAX_VALUE,
+    isError: Boolean = false,
+    errorDescription: String? = null,
+    showVisibleIcon: Boolean = false,
+    showClearIcon: Boolean = false,
+
+    ) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        label?.run {
+            DmsText(
+                text = label,
+                style = DmsTypography.Body1Medium,
+            )
+        }
+        TextField(
+            modifier = modifier,
+            value = value,
+            hint = hint,
+            onValueChange = onValueChange,
+            readOnly = readOnly,
+            singleLine = singleLine,
+            imeAction = imeAction,
+            keyboardType = keyboardType,
+            keyboardActions = keyboardActions,
+            maxLength = maxLength,
+            isError = isError,
+            showVisibleIcon = showVisibleIcon,
+            showClearIcon = showClearIcon,
+        )
+        if (isError) {
+            errorDescription?.run {
+                DmsText(
+                    text = errorDescription,
+                    style = DmsTypography.Body1Medium,
+                    color = DmsTheme.colors.outlineVariant,
+                )
+            }
+        } else {
+            description?.run {
+                DmsText(
+                    text = description,
+                    style = DmsTypography.Body1Medium,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun TextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    hint: String,
+    onValueChange: (String) -> Unit,
+    readOnly: Boolean,
+    singleLine: Boolean,
+    imeAction: ImeAction,
+    keyboardType: KeyboardType,
+    keyboardActions: KeyboardActions,
+    maxLength: Int,
+    isError: Boolean,
+    showVisibleIcon: Boolean,
+    showClearIcon: Boolean,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+
+    ) {
+    val hintAlpha by animateFloatAsState(
+        targetValue = if (value.isEmpty()) {
+            1f
+        } else {
+            0f
+        },
+        label = "",
+    )
+    var visible by remember { mutableStateOf(false) }
+    val (visualTransformation, icon) = if (visible || !showVisibleIcon) {
+        VisualTransformation.None to DmsIcon.Visibility
+    } else {
+        PasswordVisualTransformation() to DmsIcon.VisibilityOff
+    }
+    val isFocused = interactionSource.collectIsFocusedAsState().value
+    val borderStroke = BorderStroke(
+        width = 1.dp,
+        color = if (isFocused || value.isNotEmpty()) {
+            DmsTheme.colors.inversePrimary
+        } else if (isError) {
+            DmsTheme.colors.outlineVariant
+        } else {
+            DmsTheme.colors.surface
+        },
+    )
+
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        color = DmsTheme.colors.background,
+        border = borderStroke,
+    ) {
+        BasicTextField(
+            value = value.take(maxLength),
+            onValueChange = onValueChange,
+            modifier = modifier
+                .padding(16.dp),
+            textStyle = DmsTypography.Body1SemiBold,
+            singleLine = singleLine,
+            readOnly = readOnly,
+            visualTransformation = visualTransformation,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType,
+                imeAction = imeAction,
+            ),
+            keyboardActions = keyboardActions,
+            interactionSource = interactionSource,
+        ) { innerTextField ->
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Box(
+                    modifier = Modifier.weight(1f),
+                    contentAlignment = Alignment.CenterStart,
+                ) {
+                    innerTextField()
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        DmsText(
+                            modifier = Modifier.alpha(hintAlpha),
+                            text = hint,
+                            style = DmsTypography.Body1SemiBold,
+                            color = DmsTheme.colors.surface,
+                        )
+                    }
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(12.dp),
+                ) {
+                    if (showVisibleIcon) {
+                        // TODO: icon button 추가
+                    }
+                    if (showClearIcon) {
+                        // TODO: icon button 추가
+                    }
+                }
+            }
+        }
+    }
+}
