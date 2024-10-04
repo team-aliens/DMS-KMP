c9cee601b9f55ff152272cacdf33ecf6da4b4991
uiel <parkuiery@gmail.com>
2024-10-04 21:44:35 +0900

    refactor :: size 파라미터 추가

--- core/design-system/src/commonMain/kotlin/team/aliens/dms/kmp/core/designsystem/button/DmsIconButton.kt
+++ core/design-system/src/commonMain/kotlin/team/aliens/dms/kmp/core/designsystem/button/DmsIconButton.kt
@@ -1,10 +1,13 @@
 package team.aliens.dms.kmp.core.designsystem.button
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 
@@ -14,11 +17,12 @@ fun DmsIconButton(
     resource: DrawableResource,
     tint: Color,
     enabled: Boolean = true,
+    size: Dp = 24.dp,
     contentDescription: String? = null,
     onClick: () -> Unit,
 ) {
     IconButton(
-        modifier = modifier,
+        modifier = modifier.size(size),
         enabled = enabled,
         onClick = onClick,
     ) {
