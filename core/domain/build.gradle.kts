f798f509e6ca3743bf4e78aab822a31007be7ae0
uiel <parkuiery@gmail.com>
2024-10-07 18:22:12 +0900

    project :: domain 모듈 추가

---
+++ core/domain/build.gradle.kts
@@ -0,0 +1,55 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import team.aliens.dms.kmp.buildsrc.ProjectProperties
+import team.aliens.dms.kmp.buildsrc.Versions
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.ktlint)
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_17)
+                }
+            }
+        }
+    }
+
+    jvm()
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+    ).forEach {
+        it.binaries.framework {
+            baseName = "domain"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.core.jwt)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+android {
+    namespace = "team.aliens.dms.kmp.core.domain"
+    compileSdk = ProjectProperties.COMPILE_SDK
+    defaultConfig {
+        minSdk = ProjectProperties.MIN_SDK
+    }
+    compileOptions {
+        sourceCompatibility = Versions.java
+        targetCompatibility = Versions.java
+    }
+}
