70ba53685e5084f994033b43bc0be3d03c8d3311
uiel <parkuiery@gmail.com>
2024-10-08 23:19:01 +0900

    feat :: CheckStatus 추가

---
+++ core/design-system/src/commonMain/kotlin/team/aliens/dms/kmp/core/designsystem/status/CheckStatus.kt
@@ -0,0 +1,57 @@
+package team.aliens.dms.kmp.core.designsystem.status
+
+import KottieAnimation
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dmskmp.core.design_system.generated.resources.Res
+import kottieComposition.KottieCompositionSpec
+import kottieComposition.animateKottieCompositionAsState
+import kottieComposition.rememberKottieComposition
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import team.aliens.dms.kmp.core.designsystem.foundation.DmsTypography
+import team.aliens.dms.kmp.core.designsystem.text.DmsText
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+fun CheckStatus(
+    modifier: Modifier = Modifier,
+    text: String,
+) {
+    var animation by remember { mutableStateOf("") }
+
+    LaunchedEffect(Unit) {
+        animation = Res.readBytes("files/check_status.json").decodeToString()
+    }
+
+    val composition = rememberKottieComposition(
+        spec = KottieCompositionSpec.File(animation),
+    )
+    val animationState by animateKottieCompositionAsState(
+        composition = composition
+    )
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        KottieAnimation(
+            modifier = Modifier.size(80.dp),
+            composition = composition,
+            progress = { animationState.progress },
+        )
+        DmsText(
+            text = text,
+            style = DmsTypography.HeadlineSemiBold,
+        )
+    }
+}
