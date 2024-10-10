0cdfcc480bf64353f27471e464205bd9e7537b81
uiel <parkuiery@gmail.com>
2024-10-10 19:58:07 +0900

    chore :: compose 의존성 추가

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -5,6 +5,9 @@ import team.aliens.dms.kmp.buildsrc.Versions
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.ktlint)
 }
 
 kotlin {
@@ -34,6 +37,8 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(libs.lifecycle.viewmodel.compose)
+            implementation(compose.runtime)
+            implementation(compose.foundation)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
