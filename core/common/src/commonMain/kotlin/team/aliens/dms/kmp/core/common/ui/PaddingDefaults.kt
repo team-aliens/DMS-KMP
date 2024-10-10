40fa2daeac58a5d32a9c29f6f4480291a67bf065
uiel <parkuiery@gmail.com>
2024-10-10 19:57:52 +0900

    feat :: PaddingDefaults 구현

---
+++ core/common/src/commonMain/kotlin/team/aliens/dms/kmp/core/common/ui/PaddingDefaults.kt
@@ -0,0 +1,19 @@
+package team.aliens.dms.kmp.core.common.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+object PaddingDefaults {
+    val Medium = 12.dp
+    val Large = 24.dp
+}
+
+fun Modifier.horizontalPadding(
+    value: Dp = PaddingDefaults.Large,
+): Modifier = padding(horizontal = value)
+
+fun Modifier.topPadding(
+    value: Dp = PaddingDefaults.Medium,
+): Modifier = padding(top = value)
