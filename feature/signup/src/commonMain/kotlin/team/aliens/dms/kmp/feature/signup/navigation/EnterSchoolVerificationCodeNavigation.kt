8e19349443eb8afb16ab24f306c4a792d019e979
uiel <parkuiery@gmail.com>
2024-10-11 23:46:34 +0900

    feat :: EnterSchoolVerificationCodeNavigation 구현

---
+++ feature/signup/src/commonMain/kotlin/team/aliens/dms/kmp/feature/signup/navigation/EnterSchoolVerificationCodeNavigation.kt
@@ -0,0 +1,26 @@
+package team.aliens.dms.kmp.feature.signup.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import team.aliens.dms.kmp.feature.signup.model.SignUpData
+import team.aliens.dms.kmp.feature.signup.model.toJsonString
+import team.aliens.dms.kmp.feature.signup.ui.EnterSchoolVerificationCode
+
+const val NAVIGATION_ENTER_SCHOOL_VERIFICATION_CODE = "enterSchoolVerificationCode"
+
+internal fun NavGraphBuilder.enterSchoolVerificationCode(
+    onBackPressed: () -> Unit,
+    navigateToEnterSchoolVerificationQuestion: (SignUpData) -> Unit,
+) {
+    composable(NAVIGATION_ENTER_SCHOOL_VERIFICATION_CODE) {
+        EnterSchoolVerificationCode(
+            onBackPressed = onBackPressed,
+            navigateToEnterSchoolVerificationQuestion = navigateToEnterSchoolVerificationQuestion
+        )
+    }
+}
+
+fun NavController.navigateToEnterSchoolVerificationCode(signUpData: SignUpData) {
+    navigate("$NAVIGATION_ENTER_SCHOOL_VERIFICATION_CODE/${signUpData.toJsonString()}")
+}
