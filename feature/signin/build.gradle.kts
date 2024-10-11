f3b5b5079303ddbdf460a142a91015553ac79f4a
uiel <parkuiery@gmail.com>
2024-10-11 18:22:17 +0900

    chore :: 의존성 제거

--- feature/signin/build.gradle.kts
+++ feature/signin/build.gradle.kts
@@ -41,7 +41,6 @@ kotlin {
             implementation(compose.material)
             implementation(compose.material3)
             implementation(compose.ui)
-            implementation(compose.components.resources)
             implementation(libs.navigation.compose)
 
             implementation(libs.koin.core)
