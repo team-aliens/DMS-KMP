0fc76f70d8a4fb9a0009a087982c69a9f88cc004
uiel <parkuiery@gmail.com>
2024-10-11 23:45:25 +0900

    feat :: SignUpNavigation 구현

---
+++ feature/signup/src/commonMain/kotlin/team/aliens/dms/kmp/feature/signup/navigation/SignUpNavigation.kt
@@ -0,0 +1,36 @@
+package team.aliens.dms.kmp.feature.signup.navigation
+
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.navigation
+import team.aliens.dms.kmp.core.common.utils.ResourceKeys
+import team.aliens.dms.kmp.feature.signup.model.SignUpData
+import team.aliens.dms.kmp.feature.signup.model.toSignUpData
+
+const val NAVIGATION_SIGN_UP = "signUp"
+
+fun NavGraphBuilder.signUp(
+    onBackPressed: () -> Unit,
+    navigateToEnterSchoolVerificationQuestion: (SignUpData) -> Unit,
+) {
+    navigation(
+        route = NAVIGATION_SIGN_UP,
+        startDestination = NAVIGATION_ENTER_SCHOOL_VERIFICATION_CODE,
+    ) {
+        enterSchoolVerificationCode(
+            onBackPressed = onBackPressed,
+            navigateToEnterSchoolVerificationQuestion = navigateToEnterSchoolVerificationQuestion,
+        )
+    }
+}
+
+fun NavController.navigateToSignUp() {
+    navigate(NAVIGATION_SIGN_UP)
+}
+
+internal fun NavBackStackEntry.getSignUpData(): SignUpData {
+    val signUpData = arguments?.getString(ResourceKeys.SIGN_UP) ?: throw NullPointerException()
+    return signUpData.toSignUpData()
+}
+
