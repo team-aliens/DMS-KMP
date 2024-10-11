8380c4a7a656b41c027a5cfdd1b3b61a158da649
uiel <parkuiery@gmail.com>
2024-10-11 17:25:08 +0900

    refactor :: 안드로이드 koin 초기화 변경

--- composeApp/src/androidMain/kotlin/team/aliens/dms/kmp/DmsAppApplication.kt
+++ composeApp/src/androidMain/kotlin/team/aliens/dms/kmp/DmsAppApplication.kt
@@ -1,12 +1,17 @@
 package team.aliens.dms.kmp
 
 import android.app.Application
-import team.aliens.dms.kmp.di.initKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import team.aliens.dms.kmp.di.appModule
 
 class DmsAppApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        initKoin()
+        startKoin {
+            modules(appModule())
+            androidContext(this@DmsAppApplication)
+        }
     }
 }
