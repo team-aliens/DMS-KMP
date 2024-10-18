4bec1cff66efa01bdaa7229b54602154ab29e58e
uiel <parkuiery@gmail.com>
2024-10-18 18:34:41 +0900

    feat :: CountDownTimer 구현

---
+++ core/common/src/jvmMain/kotlin/team/aliens/dms/kmp/core/common/timer/CountDownTimer.jvm.kt
@@ -0,0 +1,15 @@
+package team.aliens.dms.kmp.core.common.timer
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual class CountDownTimer {
+    actual fun start(
+        durationMillis: Long,
+        intervalMillis: Long,
+        listener: CountDownTimerListener,
+    ) {
+    }
+
+    actual fun stop() {
+    }
+
+}
