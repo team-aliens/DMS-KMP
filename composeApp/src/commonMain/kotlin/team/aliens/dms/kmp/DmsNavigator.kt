1f39ca7a777f082d41287e1bfc4b16ec3f6cba41
uiel <parkuiery@gmail.com>
2024-10-20 19:19:22 +0900

    feat :: signup navigation

--- composeApp/src/commonMain/kotlin/team/aliens/dms/kmp/DmsNavigator.kt
+++ composeApp/src/commonMain/kotlin/team/aliens/dms/kmp/DmsNavigator.kt
@@ -4,14 +4,71 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import team.aliens.dms.kmp.core.network.PlatformConfig
 import team.aliens.dms.kmp.feature.signin.navigation.navigateToSignIn
+import team.aliens.dms.kmp.feature.signup.model.SignUpData
+import team.aliens.dms.kmp.feature.signup.navigation.navigateToEnterEmail
+import team.aliens.dms.kmp.feature.signup.navigation.navigateToEnterEmailVerificationCode
+import team.aliens.dms.kmp.feature.signup.navigation.navigateToEnterSchoolVerificationCode
+import team.aliens.dms.kmp.feature.signup.navigation.navigateToEnterSchoolVerificationQuestion
+import team.aliens.dms.kmp.feature.signup.navigation.navigateToSetId
+import team.aliens.dms.kmp.feature.signup.navigation.navigateToSetPassword
+import team.aliens.dms.kmp.feature.signup.navigation.navigateToSignUp
+import team.aliens.dms.kmp.feature.signup.navigation.navigateToTerms
 
 internal class DmsNavigator(
     val navController: NavHostController,
 ) {
 
-    fun navigateToLogin() {
+    fun navigateToSignIn() {
         navController.navigateToSignIn()
+    }
+
+    fun navigateToSignUp() {
+        navController.navigateToSignUp()
+    }
+
+    fun navigateToEnterSchoolVerificationCode() {
+        navController.navigateToEnterSchoolVerificationCode()
+    }
+
+    fun navigateToEnterSchoolVerificationQuestion(signUpData: SignUpData) {
+        navController.navigateToEnterSchoolVerificationQuestion(signUpData = signUpData)
+    }
+
+    fun navigateToEnterEmail(signUpData: SignUpData) {
+        navController.navigateToEnterEmail(signUpData = signUpData)
+    }
+
+    fun navigateToEnterEmailVerificationCode(signUpData: SignUpData) {
+        navController.navigateToEnterEmailVerificationCode(signUpData = signUpData)
+    }
+
+    fun navigateToSetId(signUpData: SignUpData) {
+        navController.navigateToSetId(signUpData = signUpData)
+    }
+
+    fun navigateToSetPassword(signUpData: SignUpData) {
+        navController.navigateToSetPassword(signUpData = signUpData)
+    }
+
+    fun navigateToTerms(signUpData: SignUpData) {
+        navController.navigateToTerms(signUpData = signUpData)
+    }
+
+    fun popBackStackIfNotHome() {
+        if (!isSameCurrentDestination()) {
+            popBackStack()
+        }
+    }
+
+    // TODO: private로 변경
+    fun popBackStack() {
+        navController.popBackStack()
+    }
+
+    private fun isSameCurrentDestination(): Boolean {
+        return navController.currentDestination?.route == ""
     }
 }
 
