7be2a18c3248358b82589507e7991e923f3029e5
uiel <parkuiery@gmail.com>
2024-10-20 19:11:44 +0900

    refactor :: terms url 추가

--- core/network/src/iosMain/kotlin/team/aliens/dms/kmp/core/network/PlatformConfig.ios.kt
+++ core/network/src/iosMain/kotlin/team/aliens/dms/kmp/core/network/PlatformConfig.ios.kt
@@ -2,10 +2,14 @@ package team.aliens.dms.kmp.core.network
 
 import platform.Foundation.NSBundle
 import team.aliens.dms.kmp.core.network.exception.CannotFindIosBaseurlException
+import team.aliens.dms.kmp.core.network.exception.CannotFindIosTermsUrlException
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual object PlatformConfig {
     actual val baseUrl: String =
         (NSBundle.mainBundle.objectForInfoDictionaryKey("BaseURL") as? String)
             ?: throw CannotFindIosBaseurlException()
+    actual val termsUrl: String =
+        (NSBundle.mainBundle.objectForInfoDictionaryKey("TermsUrl") as? String)
+            ?: throw CannotFindIosTermsUrlException()
 }
