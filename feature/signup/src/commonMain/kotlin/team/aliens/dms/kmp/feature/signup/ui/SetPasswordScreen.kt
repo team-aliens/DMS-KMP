56088906bfab3b271f1a52c717a1791e5e5b72a2
uiel <parkuiery@gmail.com>
2024-10-20 22:22:48 +0900

    feat :: SetPassword 구현

--- feature/signup/src/commonMain/kotlin/team/aliens/dms/kmp/feature/signup/ui/SetPasswordScreen.kt
+++ feature/signup/src/commonMain/kotlin/team/aliens/dms/kmp/feature/signup/ui/SetPasswordScreen.kt
@@ -2,6 +2,7 @@ package team.aliens.dms.kmp.feature.signup.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
@@ -9,14 +10,19 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.koin.compose.koinInject
+import team.aliens.dms.kmp.core.common.ui.PaddingDefaults
 import team.aliens.dms.kmp.core.common.ui.bottomPadding
 import team.aliens.dms.kmp.core.common.ui.horizontalPadding
+import team.aliens.dms.kmp.core.common.ui.topPadding
 import team.aliens.dms.kmp.core.designsystem.appbar.DmsLargeTopAppBar
 import team.aliens.dms.kmp.core.designsystem.button.DmsButton
 import team.aliens.dms.kmp.core.designsystem.foundation.DmsTheme
+import team.aliens.dms.kmp.core.designsystem.textfield.DmsTextField
 import team.aliens.dms.kmp.feature.signup.model.SignUpData
 import team.aliens.dms.kmp.feature.signup.viewmodel.SetPasswordSideEffect
+import team.aliens.dms.kmp.feature.signup.viewmodel.SetPasswordState
 import team.aliens.dms.kmp.feature.signup.viewmodel.SetPasswordViewModel
 
 @Composable
@@ -43,6 +49,9 @@ internal fun SetPassword(
     SetPasswordScreen(
         onBackPressed = onBackPressed,
         onNextClick = viewModel::onNextClick,
+        state = state,
+        onPasswordChange = viewModel::setPassword,
+        onCheckPasswordChange = viewModel::setPasswordCheck,
     )
 }
 
@@ -50,6 +59,9 @@ internal fun SetPassword(
 private fun SetPasswordScreen(
     onBackPressed: () -> Unit,
     onNextClick: () -> Unit,
+    state: SetPasswordState,
+    onPasswordChange: (String) -> Unit,
+    onCheckPasswordChange: (String) -> Unit,
 ) {
     Column(
         modifier = Modifier
@@ -58,9 +70,36 @@ private fun SetPasswordScreen(
     ) {
         DmsLargeTopAppBar(
             onBackPressed = onBackPressed,
-            title = "비밀번호 입력",
-            description = "비밀번호를 입력해주세요.",
+            title = "비밀번호를 입력해주세요",
+            description = "비밀번호 영문, 숫자, 기호 포함해서 8~20자가\n원칙입니다.",
         )
+        DmsTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .topPadding(PaddingDefaults.ExtraLarge)
+                .horizontalPadding(),
+            value = state.password,
+            onValueChange = onPasswordChange,
+            hint = "비밀번호를 입력해주세요",
+            label = "비밀번호 입력",
+            showVisibleIcon = true,
+            isError = state.showPasswordDescription,
+            errorDescription = "형식에 맞게 설정해주세요."
+        )
+        DmsTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .topPadding(36.dp)
+                .horizontalPadding(),
+            value = state.passwordCheck,
+            onValueChange = onCheckPasswordChange,
+            hint = "비밀번호를 다시 입력해주세요",
+            label = "비밀번호 확인",
+            showVisibleIcon = true,
+            isError = state.showCheckPasswordDescription,
+            errorDescription = "비밀번호를 확인해주세요"
+        )
+        Spacer(modifier = Modifier.weight(1f))
         DmsButton(
             modifier = Modifier
                 .fillMaxWidth()
@@ -68,6 +107,7 @@ private fun SetPasswordScreen(
                 .bottomPadding(),
             text = "다음",
             onClick = onNextClick,
+            enabled = state.buttonEnabled,
         )
     }
 }
--- feature/signup/src/commonMain/kotlin/team/aliens/dms/kmp/feature/signup/viewmodel/SetPasswordViewModel.kt
+++ feature/signup/src/commonMain/kotlin/team/aliens/dms/kmp/feature/signup/viewmodel/SetPasswordViewModel.kt
@@ -1,15 +1,47 @@
 package team.aliens.dms.kmp.feature.signup.viewmodel
 
 import team.aliens.dms.kmp.core.common.base.BaseViewModel
+import team.aliens.dms.kmp.core.common.utils.Regex
 
 
 internal class SetPasswordViewModel :
     BaseViewModel<SetPasswordState, SetPasswordSideEffect>(SetPasswordState.getDefaultState()) {
 
+    internal fun setPassword(password: String) {
+        setState {
+            state.value.copy(
+                password = password,
+                showPasswordDescription = !Regex(Regex.PASSWORD)
+                    .matches(password),
+            )
+        }
+        setButtonEnabled()
+    }
+
+    internal fun setPasswordCheck(passwordCheck: String) {
+        setState {
+            with(state.value) {
+                copy(
+                    passwordCheck = passwordCheck,
+                    showCheckPasswordDescription = password != passwordCheck,
+                )
+            }
+        }
+        setButtonEnabled()
+    }
+
+    private fun setButtonEnabled() = setState {
+        with(state.value) {
+            val hasBlank = password.isBlank() || passwordCheck.isBlank()
+            val hasInvalidation = showPasswordDescription || showCheckPasswordDescription
+            copy(buttonEnabled = !hasBlank && !hasInvalidation)
+        }
+    }
+
     internal fun onNextClick() {
         postSideEffect(
             SetPasswordSideEffect.MoveToTerms(
-                password = ""
+                password = "",
             ),
         )
     }
@@ -17,10 +49,18 @@ internal class SetPasswordViewModel :
 
 data class SetPasswordState(
     val password: String,
+    val passwordCheck: String,
+    val showPasswordDescription: Boolean,
+    val showCheckPasswordDescription: Boolean,
+    val buttonEnabled: Boolean,
 ) {
     companion object {
         fun getDefaultState() = SetPasswordState(
-            password = ""
+            password = "",
+            passwordCheck = "",
+            showPasswordDescription = false,
+            showCheckPasswordDescription = false,
+            buttonEnabled = false,
         )
     }
 }
