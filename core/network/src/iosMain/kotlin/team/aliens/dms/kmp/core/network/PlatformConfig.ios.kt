ddda9d216a04350157b9ee5b7e665c8d17292842
uiel <parkuiery@gmail.com>
2024-10-21 16:42:56 +0900

    refactor :: 파일 이름 변경

--- core/network/src/iosMain/kotlin/team/aliens/dms/kmp/core/network/PlatformConfig.ios.kt
+++ core/network/src/iosMain/kotlin/team/aliens/dms/kmp/core/network/PlatformConfig.ios.kt
@@ -1,15 +1,16 @@
 package team.aliens.dms.kmp.core.network
 
 import platform.Foundation.NSBundle
-import team.aliens.dms.kmp.core.network.exception.CannotFindIosBaseurlException
-import team.aliens.dms.kmp.core.network.exception.CannotFindIosTermsUrlException
+import team.aliens.dms.kmp.core.network.exception.CannotFindIOSBaseurlException
+import team.aliens.dms.kmp.core.network.exception.CannotFindIOSTermsUrlException
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual object PlatformConfig {
     actual val baseUrl: String =
         (NSBundle.mainBundle.objectForInfoDictionaryKey("BaseURL") as? String)
-            ?: throw CannotFindIosBaseurlException()
+            ?: throw CannotFindIOSBaseurlException()
+
     actual val termsUrl: String =
         (NSBundle.mainBundle.objectForInfoDictionaryKey("TermsUrl") as? String)
-            ?: throw CannotFindIosTermsUrlException()
+            ?: throw CannotFindIOSTermsUrlException()
 }
