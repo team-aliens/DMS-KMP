f62d6b069801ec70ad76b071d5cee4394e9b71d4
uiel <parkuiery@gmail.com>
2024-10-22 17:05:31 +0900

    refactor :: iOS webView 수정

--- core/design-system/src/iosMain/kotlin/team/aliens/dms/kmp/core/designsystem/webview/DmsWebView.ios.kt
+++ core/design-system/src/iosMain/kotlin/team/aliens/dms/kmp/core/designsystem/webview/DmsWebView.ios.kt
@@ -1,8 +1,10 @@
 package team.aliens.dms.kmp.core.designsystem.webview
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.UIKitView
 import platform.Foundation.NSMutableURLRequest
@@ -17,13 +19,18 @@ actual fun DmsWebView(
     val webView = remember { WKWebView() }
     val request = NSMutableURLRequest.requestWithURL(URL = NSURL(string = url))
 
-    webView.apply {
-        loadRequest(request)
-        allowsBackForwardNavigationGestures = true
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center,
+    ) {
+        UIKitView(
+            factory = {
+                webView.apply {
+                    allowsBackForwardNavigationGestures = true
+                }
+            },
+            modifier = Modifier.fillMaxSize(),
+        )
+        webView.loadRequest(request)
     }
-
-    UIKitView(
-        factory = { webView },
-        modifier = Modifier.fillMaxSize(),
-    )
 }
