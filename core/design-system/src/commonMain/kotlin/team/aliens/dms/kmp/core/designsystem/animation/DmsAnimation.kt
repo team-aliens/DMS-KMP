0a8cd63f7372053571e9159bc04931e3d01ab996
uiel <parkuiery@gmail.com>
2024-11-03 18:30:16 +0900

    feat :: DmsAnimation 구현

---
+++ core/design-system/src/commonMain/kotlin/team/aliens/dms/kmp/core/designsystem/animation/DmsAnimation.kt
@@ -0,0 +1,8 @@
+package team.aliens.dms.kmp.core.designsystem.animation
+
+object DmsAnimation {
+    val SplashDark = "splash_dark"
+    val SplashLight = "splash_light"
+    val CheckStatus = "check_status"
+    val ErrorStatus = "error_status"
+}
