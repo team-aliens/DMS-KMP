62949e46ccaa44d3cfbe7cd9d4a8a42c2a57e8c0
uiel <parkuiery@gmail.com>
2024-11-04 08:54:20 +0900

    feat :: ApplicationScreen 구현

---
+++ feature/application/src/commonMain/kotlin/team/aliens/dms/kmp/feature/application/ui/ApplicationScreen.kt
@@ -0,0 +1,14 @@
+package team.aliens.dms.kmp.feature.application.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun Application() {
+    ApplicationScreen()
+}
+
+@Composable
+private fun ApplicationScreen() {
+    Text("신청")
+}
