acafe5e7754787ead37585bc507c8dc7e7ad0492
uiel <parkuiery@gmail.com>
2024-11-04 09:22:50 +0900

    feat :: NoticeScreen 구현

---
+++ feature/notice/src/commonMain/kotlin/team/aliens/dms/kmp/feature/notice/ui/NoticeScreen.kt
@@ -0,0 +1,14 @@
+package team.aliens.dms.kmp.feature.notice.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun Notice() {
+    NoticeScreen()
+}
+
+@Composable
+private fun NoticeScreen() {
+    Text("공지")
+}
