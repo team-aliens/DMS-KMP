0cd24cacf13b0b03496838580eba60e73a22ec05
uiel <parkuiery@gmail.com>
2024-11-27 15:06:28 +0900

    refactor :: 로고 및 actions 추가

--- core/design-system/src/commonMain/kotlin/team/aliens/dms/kmp/core/designsystem/appbar/DmsTopAppBar.kt
+++ core/design-system/src/commonMain/kotlin/team/aliens/dms/kmp/core/designsystem/appbar/DmsTopAppBar.kt
@@ -1,16 +1,20 @@
 package team.aliens.dms.kmp.core.designsystem.appbar
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.painterResource
 import team.aliens.dms.kmp.core.designsystem.button.DmsIconButton
 import team.aliens.dms.kmp.core.designsystem.foundation.DmsIcon
 import team.aliens.dms.kmp.core.designsystem.foundation.DmsTheme
@@ -20,8 +24,10 @@ import team.aliens.dms.kmp.core.designsystem.text.DmsText
 @Composable
 fun DmsTopAppBar(
     modifier: Modifier = Modifier,
+    showLogo: Boolean = false,
     onBackPressed: (() -> Unit)? = null,
-    title: String,
+    actions: (@Composable RowScope.() -> Unit)? = null,
+    title: String? = null,
 ) {
     Box(
         modifier = modifier
@@ -33,8 +39,21 @@ fun DmsTopAppBar(
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
+            horizontalArrangement = if (showLogo || onBackPressed != null) Arrangement.SpaceBetween else Arrangement.End,
             verticalAlignment = Alignment.CenterVertically,
         ) {
+            if (showLogo) {
+                Image(
+                    painter = painterResource(
+                        if (isSystemInDarkTheme()) {
+                            DmsIcon.SymbolDark
+                        } else {
+                            DmsIcon.SymbolLight
+                        },
+                    ),
+                    contentDescription = null,
+                )
+            }
             onBackPressed?.let {
                 DmsIconButton(
                     resource = DmsIcon.ArrowBack,
@@ -42,11 +61,20 @@ fun DmsTopAppBar(
                     onClick = it,
                 )
             }
+            actions?.let {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    content = it,
+                )
+            }
         }
-        DmsText(
-            text = title,
-            style = DmsTypography.SubtitleSemiBold,
-        )
+        title?.let {
+            DmsText(
+                text = it,
+                style = DmsTypography.SubtitleSemiBold,
+            )
+        }
     }
 }
 
