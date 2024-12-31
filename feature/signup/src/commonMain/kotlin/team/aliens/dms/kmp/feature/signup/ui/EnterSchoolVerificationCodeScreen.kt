8b8c09568ccd206cc1760985f58c01aeeda14e47
uiel <parkuiery@gmail.com>
2024-12-31 22:06:44 +0900

    refactor :: 회원가입 화면 제거

--- feature/signup/src/commonMain/kotlin/team/aliens/dms/kmp/feature/signup/ui/EnterSchoolVerificationCodeScreen.kt
+++ feature/signup/src/commonMain/kotlin/team/aliens/dms/kmp/feature/signup/ui/EnterSchoolVerificationCodeScreen.kt
@@ -2,23 +2,14 @@ package team.aliens.dms.kmp.feature.signup.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import org.koin.compose.koinInject
-import team.aliens.dms.kmp.core.common.ui.PaddingDefaults
-import team.aliens.dms.kmp.core.common.ui.bottomPadding
-import team.aliens.dms.kmp.core.common.ui.horizontalPadding
-import team.aliens.dms.kmp.core.common.ui.topPadding
-import team.aliens.dms.kmp.core.designsystem.appbar.DmsLargeTopAppBar
-import team.aliens.dms.kmp.core.designsystem.button.DmsButton
 import team.aliens.dms.kmp.core.designsystem.foundation.DmsTheme
-import team.aliens.dms.kmp.core.designsystem.numberfield.DmsNumberField
 import team.aliens.dms.kmp.feature.signup.model.SignUpData
 import team.aliens.dms.kmp.feature.signup.viewmodel.EnterSchoolVerificationCodeSideEffect
 import team.aliens.dms.kmp.feature.signup.viewmodel.EnterSchoolVerificationCodeState
@@ -64,29 +55,6 @@ private fun EnterSchoolVerificationCodeScreen(
             .fillMaxSize()
             .background(DmsTheme.colors.background),
     ) {
-        DmsLargeTopAppBar(
-            onBackPressed = onBackPressed,
-            title = "인증코드를 입력해주세요",
-            description = "학교 인증코드를 입력해주세요.",
-        )
-        DmsNumberField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .topPadding(PaddingDefaults.ExtraLarge)
-                .horizontalPadding(),
-            value = state.verificationCode,
-            onValueChange = onVerificationCodeChange,
-            totalLength = VERIFICATION_CODE_LENGTH,
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        DmsButton(
-            modifier = Modifier
-                .fillMaxWidth()
-                .horizontalPadding()
-                .bottomPadding(),
-            text = "다음",
-            onClick = onNextClick,
-            enabled = state.buttonEnabled,
-        )
+
     }
 }
--- feature/signup/src/commonMain/kotlin/team/aliens/dms/kmp/feature/signup/ui/TermsScreen.kt
+++ feature/signup/src/commonMain/kotlin/team/aliens/dms/kmp/feature/signup/ui/TermsScreen.kt
@@ -1,39 +1,15 @@
 package team.aliens.dms.kmp.feature.signup.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.unit.dp
-import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.koinInject
-import team.aliens.dms.kmp.core.common.ui.PaddingDefaults
-import team.aliens.dms.kmp.core.common.ui.bottomPadding
-import team.aliens.dms.kmp.core.common.ui.horizontalPadding
-import team.aliens.dms.kmp.core.common.ui.topPadding
-import team.aliens.dms.kmp.core.designsystem.appbar.DmsLargeTopAppBar
-import team.aliens.dms.kmp.core.designsystem.button.DmsButton
-import team.aliens.dms.kmp.core.designsystem.foundation.DmsIcon
 import team.aliens.dms.kmp.core.designsystem.foundation.DmsTheme
-import team.aliens.dms.kmp.core.designsystem.foundation.DmsTypography
-import team.aliens.dms.kmp.core.designsystem.text.DmsText
-import team.aliens.dms.kmp.core.designsystem.webview.DmsWebView
 import team.aliens.dms.kmp.feature.signup.viewmodel.TermsState
 import team.aliens.dms.kmp.feature.signup.viewmodel.TermsViewModel
 
@@ -75,78 +51,6 @@ private fun TermsScreen(
             .fillMaxSize()
             .background(DmsTheme.colors.background),
     ) {
-        DmsLargeTopAppBar(
-            onBackPressed = onBackPressed,
-            title = "개인정보처리방침 안내",
-            description = "개인정보처리방침을 확인해주세요.",
-        )
-        AllAgreeButton(
-            buttonEnabled = state.buttonEnabled,
-            onAllAgreeButtonClick = onAllAgreeButtonClick,
-        )
-        DmsWebView(
-            modifier = Modifier
-                .weight(1f)
-                .topPadding(PaddingDefaults.Medium)
-                .horizontalPadding()
-                .clip(RoundedCornerShape(8.dp)),
-            url = "$termsUrl/policy/privacy?theme=$theme",
-        )
-        DmsButton(
-            modifier = Modifier
-                .fillMaxWidth()
-                .topPadding(PaddingDefaults.ExtraLarge)
-                .horizontalPadding()
-                .bottomPadding(),
-            text = "다음",
-            onClick = navigateToSignIn,
-            enabled = state.buttonEnabled,
-        )
-    }
-}
 
-@Composable
-private fun AllAgreeButton(
-    buttonEnabled: Boolean,
-    onAllAgreeButtonClick: (Boolean) -> Unit,
-) {
-    var isCheck by remember { mutableStateOf(buttonEnabled) }
-    val (background, contentColor) = if (isCheck) {
-        DmsTheme.colors.onSecondary to DmsTheme.colors.surfaceContainerHighest
-    } else {
-        DmsTheme.colors.onBackground to DmsTheme.colors.onSurface
-    }
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .topPadding(PaddingDefaults.ExtraLarge)
-            .horizontalPadding()
-            .background(
-                color = background,
-                shape = RoundedCornerShape(8.dp),
-            )
-            .clickable(
-                interactionSource = null,
-                indication = null,
-                onClick = {
-                    isCheck = !isCheck
-                    onAllAgreeButtonClick(isCheck)
-                },
-            )
-            .padding(PaddingDefaults.Large),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-    ) {
-        Icon(
-            painter = painterResource(DmsIcon.Check),
-            contentDescription = "check",
-            tint = contentColor,
-        )
-        DmsText(
-            text = "전체 동의",
-            style = DmsTypography.SubtitleSemiBold,
-            color = contentColor,
-        )
     }
 }
