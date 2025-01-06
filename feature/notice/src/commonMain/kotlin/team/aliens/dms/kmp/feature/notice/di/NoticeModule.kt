2af35a1b67923eced67d5775eed87220af2558b5
uiel <parkuiery@gmail.com>
2025-01-06 23:16:28 +0900

    feat :: NoticeDetails 구현

--- feature/notice/src/commonMain/kotlin/team/aliens/dms/kmp/feature/notice/di/NoticeModule.kt
+++ feature/notice/src/commonMain/kotlin/team/aliens/dms/kmp/feature/notice/di/NoticeModule.kt
@@ -2,8 +2,10 @@ package team.aliens.dms.kmp.feature.notice.di
 
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
+import team.aliens.dms.kmp.feature.notice.viewmodel.NoticeDetailsViewModel
 import team.aliens.dms.kmp.feature.notice.viewmodel.NoticesViewModel
 
 val noticeModule = module {
     viewModelOf(::NoticesViewModel)
+    viewModelOf(::NoticeDetailsViewModel)
 }
