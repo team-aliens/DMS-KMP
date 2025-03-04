7563d01b588ed25177086a1666e5c75f02f6712e
uiel <parkuiery@gmail.com>
2025-03-04 09:36:42 +0900

    delete :: core:jwt module

--- core/jwt/src/commonMain/kotlin/team/aliens/dms/kmp/core/jwt/di/DataSourceModule.kt
+++
@@ -1,9 +0,0 @@
-package team.aliens.dms.kmp.core.jwt.di
-
-import org.koin.dsl.module
-import team.aliens.dms.kmp.core.jwt.datastore.JwtDataStoreDataSource
-import team.aliens.dms.kmp.core.jwt.datastore.JwtDataStoreDataSourceImpl
-
-internal val dataSourceModule = module {
-    single<JwtDataStoreDataSource> { JwtDataStoreDataSourceImpl(get()) }
-}
