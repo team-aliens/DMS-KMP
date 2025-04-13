e8e96eb76061e2cc1d8ae8e1b8492292673d8941
uiel <parkuiery@gmail.com>
2025-04-13 14:50:38 +0900

    refactor :: core:database DataSourceModule 변경

---
+++ core/database/src/commonMain/kotlin/team/aliens/dms/kmp/core/database/di/DataSourceModule.kt
@@ -0,0 +1,14 @@
+package team.aliens.dms.kmp.core.database.di
+
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.module
+import team.aliens.dms.kmp.core.database.datasource.meal.MealDatabaseDataSource
+import team.aliens.dms.kmp.core.database.datasource.meal.MealDatabaseDataSourceImpl
+import team.aliens.dms.kmp.core.database.datasource.notice.NoticeDatabaseDataSource
+import team.aliens.dms.kmp.core.database.datasource.notice.NoticeDatabaseDataSourceImpl
+
+internal val dataSourceModule = module {
+    singleOf(::MealDatabaseDataSourceImpl) { bind<MealDatabaseDataSource>() }
+    singleOf(::NoticeDatabaseDataSourceImpl) { bind<NoticeDatabaseDataSource>() }
+}
