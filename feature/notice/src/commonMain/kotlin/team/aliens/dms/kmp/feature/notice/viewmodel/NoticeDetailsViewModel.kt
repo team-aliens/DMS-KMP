9b6fc54028579ed62a4399f764a34e21fe67c5f3
uiel <parkuiery@gmail.com>
2025-04-18 09:18:55 +0900

    feat :: feature:notice 공지사항 상세 api 연동

--- feature/notice/src/commonMain/kotlin/team/aliens/dms/kmp/feature/notice/viewmodel/NoticeDetailsViewModel.kt
+++ feature/notice/src/commonMain/kotlin/team/aliens/dms/kmp/feature/notice/viewmodel/NoticeDetailsViewModel.kt
@@ -1,9 +1,23 @@
 package team.aliens.dms.kmp.feature.notice.viewmodel
 
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import team.aliens.dms.kmp.core.common.base.BaseViewModel
+import team.aliens.dms.kmp.core.domain.usecase.notice.GetNoticeDetailUseCase
 
-internal class NoticeDetailsViewModel :
-    BaseViewModel<NoticeDetailsState, NoticeDetailsSideEffect>(NoticeDetailsState.getDefaultState())
+internal class NoticeDetailsViewModel(
+    private val getNoticeDetailUseCase: GetNoticeDetailUseCase,
+) :
+    BaseViewModel<NoticeDetailsState, NoticeDetailsSideEffect>(NoticeDetailsState.getDefaultState()) {
+
+    internal fun getNoticeDetail(noticeId: String) {
+        viewModelScope.launch {
+            getNoticeDetailUseCase(noticeId = noticeId).onSuccess {
+
+            }
+        }
+    }
+}
 
 internal data class NoticeDetailsState(
     val s: String,
