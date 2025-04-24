902c4d8c95efcd76cda9bcd309701fb90ee831a1
uiel <parkuiery@gmail.com>
2025-04-24 14:50:36 +0900

    refactor :: feature:outing navigation 변경

--- feature/outing/src/commonMain/kotlin/team/aliens/dms/kmp/feature/outing/navigation/OutingApplicationNavigation.kt
+++ feature/outing/src/commonMain/kotlin/team/aliens/dms/kmp/feature/outing/navigation/OutingApplicationNavigation.kt
@@ -2,17 +2,23 @@ package team.aliens.dms.kmp.feature.outing.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import kotlinx.serialization.Serializable
 import team.aliens.dms.kmp.feature.outing.OutingApplication
 
-const val NAVIGATION_OUTING_APPLICATION = "outingApplication"
+@Serializable
+data object OutingApplicationRoute
+
+fun NavController.navigateToOutingApplication(
+    navOptions: NavOptions? = null,
+) = navigate(
+    route = OutingApplicationRoute,
+    navOptions = navOptions,
+)
 
 fun NavGraphBuilder.outingApplication() {
-    composable(NAVIGATION_OUTING_APPLICATION) {
+    composable<OutingApplicationRoute> {
         OutingApplication()
     }
-}
-
-fun NavController.navigateToOutingApplication() {
-    navigate(NAVIGATION_OUTING_APPLICATION)
 }
