33fc2fad685862a49317b70daeeada84222e0ffd
uiel <parkuiery@gmail.com>
2025-05-30 11:21:17 +0900

    feat :: core:model remains model 구현

---
+++ core/model/src/commonMain/kotlin/team/aliens/dms/kmp/core/model/remains/AppliedRemainsOptionModel.kt
@@ -0,0 +1,9 @@
+package team.aliens.dms.kmp.core.model.remains
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AppliedRemainsOptionModel(
+    val id: String,
+    val title: String,
+)
