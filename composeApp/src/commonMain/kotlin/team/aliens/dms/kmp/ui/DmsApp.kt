0e911126ef03945cdd50fcba570c861bb65e7ac8
uiel <parkuiery@gmail.com>
2025-06-16 16:01:03 +0900

    feat :: DmsApp snackbar 추가

--- composeApp/src/commonMain/kotlin/team/aliens/dms/kmp/ui/DmsApp.kt
+++ composeApp/src/commonMain/kotlin/team/aliens/dms/kmp/ui/DmsApp.kt
@@ -1,26 +1,52 @@
 package team.aliens.dms.kmp.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.navigation.compose.NavHost
 import team.aliens.dms.kmp.core.designsystem.foundation.DmsTheme
+import team.aliens.dms.kmp.core.designsystem.snackbar.DmsSnackBar
+import team.aliens.dms.kmp.core.designsystem.snackbar.DmsSnackBarVisuals
 import team.aliens.dms.kmp.navigation.authorized.AuthRoute
 import team.aliens.dms.kmp.navigation.authorized.authGraph
 import team.aliens.dms.kmp.navigation.main.mainGraph
 
 @Composable
 internal fun DmsApp(
+    modifier: Modifier = Modifier,
     appState: DmsAppState = rememberDmsAppState(),
 ) {
-    DmsTheme {
+    Scaffold(
+        modifier = modifier,
+        contentWindowInsets = WindowInsets(0.dp),
+        snackbarHost = {
+            SnackbarHost(
+                modifier = Modifier
+                    .zIndex(2f)
+                    .padding(bottom = 100.dp),
+                hostState = appState.snackBarHostState,
+                snackbar = {
+                    val visuals = it.visuals as DmsSnackBarVisuals
+                    DmsSnackBar(
+                        snackBarType = visuals.snackBarType,
+                        message = visuals.message,
+                    )
+                }
+            )
+        },
+    ) { innerPadding ->
         NavHost(
             modifier = Modifier
                 .background(DmsTheme.colors.background)
-                .navigationBarsPadding()
-                .statusBarsPadding(),
+                .padding(innerPadding),
             navController = appState.navController,
             startDestination = AuthRoute,
         ) {
