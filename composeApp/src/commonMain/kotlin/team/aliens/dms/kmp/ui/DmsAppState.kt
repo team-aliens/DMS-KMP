c751591aa092599abf9c86f81c06c1565e51176b
uiel <parkuiery@gmail.com>
2025-06-16 16:00:40 +0900

    feat :: DmsAppState snackBar 추가

--- composeApp/src/commonMain/kotlin/team/aliens/dms/kmp/ui/DmsAppState.kt
+++ composeApp/src/commonMain/kotlin/team/aliens/dms/kmp/ui/DmsAppState.kt
@@ -1,21 +1,68 @@
 package team.aliens.dms.kmp.ui
 
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarVisuals
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import team.aliens.dms.kmp.core.designsystem.snackbar.DmsSnackBarType
+import team.aliens.dms.kmp.core.designsystem.snackbar.DmsSnackBarVisuals
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 @Composable
 fun rememberDmsAppState(
     navController: NavHostController = rememberNavController(),
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    snackBarHostState: SnackbarHostState = remember { SnackbarHostState() },
 ): DmsAppState {
-    return remember(navController) {
-        DmsAppState(navController = navController)
+    return remember(
+        navController,
+        coroutineScope,
+        snackBarHostState,
+    ) {
+        DmsAppState(
+            navController = navController,
+            coroutineScope = coroutineScope,
+            snackBarHostState = snackBarHostState,
+        )
     }
 }
 
 @Stable
 class DmsAppState(
     val navController: NavHostController,
-)
+    val coroutineScope: CoroutineScope,
+    val snackBarHostState: SnackbarHostState,
+) {
+    private fun showSnackBar(
+        visuals: DmsSnackBarVisuals,
+        duration: Duration = 2.seconds,
+    ) {
+        coroutineScope.launch {
+            val job = launch {
+                snackBarHostState.showSnackbar(visuals)
+            }
+            delay(duration)
+            job.cancel()
+        }
+    }
+
+    fun showSnackBar(
+        snackBarType: DmsSnackBarType,
+        message: String
+    ) {
+        showSnackBar(
+            visuals = DmsSnackBarVisuals(
+                snackBarType = snackBarType,
+                message = message,
+            )
+        )
+    }
+}
