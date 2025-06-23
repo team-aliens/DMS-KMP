a8c7f2a23be5cb53e0bfbf99a180a16ebadb4a2e
uiel <parkuiery@gmail.com>
2025-06-23 23:14:19 +0900

    feat :: core:common NavType vote navtype 추가

--- core/common/src/commonMain/kotlin/team/aliens/dms/kmp/core/common/navtype/NavType.kt
+++ core/common/src/commonMain/kotlin/team/aliens/dms/kmp/core/common/navtype/NavType.kt
@@ -1,6 +1,11 @@
 package team.aliens.dms.kmp.core.common.navtype
 
 import team.aliens.dms.kmp.core.model.signup.SignUpData
+import team.aliens.dms.kmp.core.model.type.VoteType
+import team.aliens.dms.kmp.core.model.votes.VoteModel
 import kotlin.reflect.typeOf
 
+val VoteTypeNavType = typeOf<VoteType>() to serializableType<VoteType>()
+
 val SignUpDataNavType = typeOf<SignUpData>() to serializableType<SignUpData>()
+val VoteNavType = typeOf<VoteModel>() to serializableType<VoteModel>()
