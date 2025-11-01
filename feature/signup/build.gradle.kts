bdb613ebbd45b54995a1dab1d6a6968ac986d5d9
uiel <parkuiery@gmail.com>
2025-11-01 21:42:55 +0900

    chore :: feature:signup 의존성 추가

--- feature/signup/build.gradle.kts
+++ feature/signup/build.gradle.kts
@@ -53,6 +53,8 @@ kotlin {
             implementation(projects.core.common)
             implementation(projects.core.designSystem)
             implementation(projects.core.model)
+            implementation(projects.core.domain)
+            implementation(projects.core.util)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
