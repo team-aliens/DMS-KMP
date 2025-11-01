787b5d8486a54bf230599351ff3e4eb391a61994
uiel <parkuiery@gmail.com>
2025-11-01 21:48:24 +0900

    refactor :: feature:splash 디자인 수정

--- feature/splash/src/commonMain/kotlin/team/aliens/dms/kmp/feature/splash/ui/SplashScreen.kt
+++ feature/splash/src/commonMain/kotlin/team/aliens/dms/kmp/feature/splash/ui/SplashScreen.kt
@@ -1,48 +1,35 @@
 package team.aliens.dms.kmp.feature.splash.ui
 
-import KottieAnimation
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import dmskmp.feature.splash.generated.resources.Res
-import kottieComposition.KottieCompositionSpec
-import kottieComposition.animateKottieCompositionAsState
-import kottieComposition.rememberKottieComposition
-import org.jetbrains.compose.resources.ExperimentalResourceApi
+import io.github.alexzhirkevich.compottie.LottieCompositionSpec
+import io.github.alexzhirkevich.compottie.animateLottieCompositionAsState
+import io.github.alexzhirkevich.compottie.rememberLottieComposition
+import io.github.alexzhirkevich.compottie.rememberLottiePainter
 import org.koin.compose.viewmodel.koinViewModel
 import team.aliens.dms.kmp.core.designsystem.foundation.DmsTheme
 import team.aliens.dms.kmp.feature.splash.viewmodel.SplashSideEffect
 import team.aliens.dms.kmp.feature.splash.viewmodel.SplashViewModel
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 internal fun Splash(
     navigateToLogin: () -> Unit,
     navigateToMain: () -> Unit,
 ) {
     val viewModel: SplashViewModel = koinViewModel()
-    var animation by remember { mutableStateOf("") }
-
-    val file = if (isSystemInDarkTheme()) {
-        "files/splash_dark.json"
-    } else {
-        "files/splash_light.json"
-    }
-
-    LaunchedEffect(Unit) {
-        animation = Res.readBytes(file).decodeToString()
-    }
 
     LaunchedEffect(Unit) {
         viewModel.sideEffect.collect {
@@ -53,30 +40,38 @@ internal fun Splash(
         }
     }
 
-    SplashScreen(animation = animation)
+    SplashScreen()
 }
 
 @Composable
-private fun SplashScreen(
-    animation: String,
-) {
-    val composition = rememberKottieComposition(
-        spec = KottieCompositionSpec.File(animation),
-    )
-    val animationState by animateKottieCompositionAsState(
-        composition = composition,
-    )
+private fun SplashScreen() {
+    val file = if (isSystemInDarkTheme()) {
+        "files/splash_dark.json"
+    } else {
+        "files/splash_light.json"
+    }
+    val composition by rememberLottieComposition {
+        LottieCompositionSpec.JsonString(
+            Res.readBytes(file).decodeToString(),
+        )
+    }
+    val progress by animateLottieCompositionAsState(composition)
+
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(DmsTheme.colors.background),
+            .background(DmsTheme.colors.background)
+            .statusBarsPadding()
+            .navigationBarsPadding(),
         contentAlignment = Alignment.Center,
     ) {
-        KottieAnimation(
-            modifier = Modifier
-                .padding(horizontal = 120.dp),
-            composition = composition,
-            progress = { animationState.progress },
+        Image(
+            modifier = Modifier.padding(horizontal = 120.dp),
+            painter = rememberLottiePainter(
+                composition = composition,
+                progress = { progress },
+            ),
+            contentDescription = null,
         )
     }
 }
