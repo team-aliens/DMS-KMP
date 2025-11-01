d7f08d251ab190227c57cd219460bee4ea966c20
uiel <parkuiery@gmail.com>
2025-11-01 21:42:36 +0900

    refactor :: feature:signin 디자인 수정

--- feature/signin/src/commonMain/kotlin/team/aliens/dms/kmp/feature/signin/ui/SignInScreen.kt
+++ feature/signin/src/commonMain/kotlin/team/aliens/dms/kmp/feature/signin/ui/SignInScreen.kt
@@ -4,12 +4,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -76,16 +79,27 @@ private fun SignInScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(DmsTheme.colors.background),
+            .background(DmsTheme.colors.surfaceTint)
+            .statusBarsPadding()
+            .navigationBarsPadding(),
     ) {
-        DmsTopAppBar(title = "로그인")
         DmsSymbol(
             modifier = Modifier
-                .startPadding(24.dp)
-                .topPadding(40.dp),
+                .topPadding(52.dp)
+                .horizontalPadding(24.dp),
+        )
+        DmsText(
+            modifier = Modifier
+                .padding(top = 20.dp)
+                .horizontalPadding(24.dp),
+            text = "로그인",
+            style = DmsTypography.TitleB,
+            color = DmsTheme.colors.onTertiaryContainer,
         )
         UserInformationInputs(
-            modifier = Modifier.topPadding(44.dp),
+            modifier = Modifier
+                .topPadding(48.dp)
+                .horizontalPadding(24.dp),
             accountId = state.accountId,
             onAccountIdChange = onAccountIdChange,
             password = state.password,
@@ -97,13 +111,14 @@ private fun SignInScreen(
         SignupActions(onSignUp = navigateToSignUp)
         DmsButton(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(24.dp),
+                .fillMaxWidth(),
             text = "로그인",
             buttonType = ButtonType.Contained,
             buttonColor = ButtonColor.Primary,
+            keyboardInteractionEnabled = true,
             onClick = onSignInClick,
             enabled = state.buttonEnabled,
+            isLoading = state.isLoading,
         )
     }
 }
@@ -119,47 +134,47 @@ private fun UserInformationInputs(
     onResetPassword: () -> Unit,
 ) {
     Column(
-        modifier = modifier.horizontalPadding(24.dp),
-        verticalArrangement = Arrangement.spacedBy(36.dp),
+        modifier = modifier.fillMaxWidth(),
     ) {
         DmsTextField(
             modifier = Modifier.fillMaxWidth(),
+            label = "아이디",
             value = accountId,
-            hint = "아이디",
+            hint = "아이디 입력",
             onValueChange = onAccountIdChange,
         )
         DmsTextField(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 32.dp),
+            label = "비밀번호",
             value = password,
-            hint = "비빌번호",
+            hint = "비빌번호 입력",
             onValueChange = onPasswordChange,
             showVisibleIcon = true,
         )
         Row(
-            modifier = Modifier.align(Alignment.End),
-            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            modifier = Modifier
+                .align(Alignment.End)
+                .padding(top = 24.dp),
+            verticalAlignment = Alignment.CenterVertically,
         ) {
-            DmsText(
-                modifier = Modifier.clickable(
-                    onClick = onFindId,
-                ),
+            DmsButton(
                 text = "아이디 찾기",
-                style = DmsTypography.Body3,
-                color = DmsTheme.colors.inverseSurface,
+                buttonType = ButtonType.Text,
+                buttonColor = ButtonColor.Gray,
+                onClick = onFindId,
             )
             VerticalDivider(
                 modifier = Modifier.height(12.dp),
-                thickness = 0.5.dp,
+                thickness = 1.dp,
                 color = DmsTheme.colors.inverseSurface,
             )
-            DmsText(
-                modifier = Modifier
-                    .clickable(
-                        onClick = onResetPassword,
-                    ),
+            DmsButton(
                 text = "비밀번호 재설정",
-                style = DmsTypography.Body3,
-                color = DmsTheme.colors.inverseSurface,
+                buttonType = ButtonType.Text,
+                buttonColor = ButtonColor.Gray,
+                onClick = onResetPassword,
             )
         }
     }
@@ -173,21 +188,21 @@ private fun SignupActions(
     Row(
         modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.spacedBy(
-            space = 12.dp,
+            space = 4.dp,
             alignment = Alignment.CenterHorizontally,
         ),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         DmsText(
             text = "아직 회원이 아니신가요?",
-            style = DmsTypography.Caption,
+            style = DmsTypography.labelM,
             color = DmsTheme.colors.inverseSurface,
         )
-        DmsText(
-            modifier = Modifier.clickable(onClick = onSignUp),
-            text = "회원가입",
-            style = DmsTypography.Body1,
-            color = DmsTheme.colors.onSecondary,
+        DmsButton(
+            text = "회원가입 하러가기",
+            buttonType = ButtonType.Underline,
+            buttonColor = ButtonColor.Gray,
+            onClick = onSignUp,
         )
     }
 }
