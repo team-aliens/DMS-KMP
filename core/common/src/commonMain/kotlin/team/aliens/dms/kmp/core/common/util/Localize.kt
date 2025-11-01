cc08ba031eadb332d659e5a9a28c51987d376a92
uiel <parkuiery@gmail.com>
2025-11-01 20:50:44 +0900

    feat :: core:common Localize추가

---
+++ core/common/src/commonMain/kotlin/team/aliens/dms/kmp/core/common/util/Localize.kt
@@ -0,0 +1,14 @@
+package team.aliens.dms.kmp.core.common.util
+
+import kotlinx.datetime.DayOfWeek
+
+fun DayOfWeek.toKorean(): String = when (this) {
+    DayOfWeek.MONDAY -> "월"
+    DayOfWeek.TUESDAY -> "화"
+    DayOfWeek.WEDNESDAY -> "수"
+    DayOfWeek.THURSDAY -> "목"
+    DayOfWeek.FRIDAY -> "금"
+    DayOfWeek.SATURDAY -> "토"
+    DayOfWeek.SUNDAY -> "일"
+    else -> ""
+}
