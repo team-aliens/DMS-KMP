052beed9802080cb6c74cede4e498c7659f698fd
uiel <parkuiery@gmail.com>
2025-11-01 20:52:01 +0900

    delete :: core:common 파일 삭제

--- core/common/src/commonMain/kotlin/team/aliens/dms/kmp/core/common/utils/Regex.kt
+++
@@ -1,5 +0,0 @@
-package team.aliens.dms.kmp.core.common.utils
-
-object Regex {
-    const val PASSWORD = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@\$!%*#?&])[A-Za-z\\d@\$!%*#?&]{8,20}\$"
-}
--- core/common/src/commonMain/kotlin/team/aliens/dms/kmp/core/common/utils/ResourceKeys.kt
+++
@@ -1,5 +0,0 @@
-package team.aliens.dms.kmp.core.common.utils
-
-object ResourceKeys {
-    const val SIGN_UP = "signUp"
-}
