fce3e03223165ecd6995275b73aae030337ee067
uiel <parkuiery@gmail.com>
2025-11-01 20:47:54 +0900

    feat :: MainViewController theme 추가

--- composeApp/src/iosMain/kotlin/team/aliens/dms/kmp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/team/aliens/dms/kmp/MainViewController.kt
@@ -1,6 +1,11 @@
 package team.aliens.dms.kmp
 
 import androidx.compose.ui.window.ComposeUIViewController
+import team.aliens.dms.kmp.core.designsystem.foundation.DmsTheme
 import team.aliens.dms.kmp.ui.DmsApp
 
-fun mainViewController() = ComposeUIViewController { DmsApp() }
+fun mainViewController() = ComposeUIViewController {
+    DmsTheme {
+        DmsApp()
+    }
+}
