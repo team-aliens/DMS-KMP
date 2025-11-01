9d27efe9f1137fee5cf259a861398c384ebff010
uiel <parkuiery@gmail.com>
2025-11-01 21:39:07 +0900

    refactor :: feature:notice 디자인 수정

--- feature/notice/src/commonMain/kotlin/team/aliens/dms/kmp/feature/notice/navigation/NoticesNavigation.kt
+++ feature/notice/src/commonMain/kotlin/team/aliens/dms/kmp/feature/notice/navigation/NoticesNavigation.kt
@@ -1,6 +1,8 @@
 package team.aliens.dms.kmp.feature.notice.navigation
 
+import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import kotlinx.serialization.Serializable
 import team.aliens.dms.kmp.feature.notice.ui.Notices
@@ -8,11 +10,20 @@ import team.aliens.dms.kmp.feature.notice.ui.Notices
 @Serializable
 data object NoticesRoute
 
+fun NavController.navigateToNotices(
+    navOptions: NavOptions? = null,
+) = navigate(
+    route = NoticesRoute,
+    navOptions = navOptions,
+)
+
 fun NavGraphBuilder.notices(
+    onNavigateBack: () -> Unit,
     onNoticeDetailClick: (String) -> Unit,
 ) {
     composable<NoticesRoute> {
         Notices(
+            onNavigateBack = onNavigateBack,
             onNoticeDetailClick = onNoticeDetailClick,
         )
     }
--- feature/notice/src/commonMain/kotlin/team/aliens/dms/kmp/feature/notice/ui/NoticeDetailScreen.kt
+++ feature/notice/src/commonMain/kotlin/team/aliens/dms/kmp/feature/notice/ui/NoticeDetailScreen.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -13,6 +16,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.koin.compose.viewmodel.koinViewModel
+import team.aliens.dms.kmp.core.common.ui.horizontalPadding
+import team.aliens.dms.kmp.core.common.ui.topPadding
+import team.aliens.dms.kmp.core.common.ui.verticalPadding
 import team.aliens.dms.kmp.core.designsystem.appbar.DmsTopAppBar
 import team.aliens.dms.kmp.core.designsystem.foundation.DmsTheme
 import team.aliens.dms.kmp.core.designsystem.foundation.DmsTypography
@@ -42,7 +48,9 @@ private fun NoticeDetailsScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(DmsTheme.colors.background),
+            .background(DmsTheme.colors.background)
+            .statusBarsPadding()
+            .navigationBarsPadding(),
     ) {
         DmsTopAppBar(
             title = "안내",
@@ -62,28 +70,32 @@ private fun Notice(
     Column(
         modifier = modifier
             .fillMaxWidth()
+            .horizontalPadding(10.dp)
+            .background(color = DmsTheme.colors.surfaceTint, shape = RoundedCornerShape(32.dp))
             .padding(24.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp),
     ) {
         DmsText(
             text = notice.title,
-            style = DmsTypography.Header3,
-            color = DmsTheme.colors.surfaceBright,
+            style = DmsTypography.LBodyB,
+            color = DmsTheme.colors.tertiaryContainer,
         )
         DmsText(
+            modifier = Modifier.topPadding(8.dp),
             text = "${notice.createdAt.date} ${notice.createdAt.time}",
-            style = DmsTypography.Body1,
-            color = DmsTheme.colors.inverseSurface,
+            style = DmsTypography.labelM,
+            color = DmsTheme.colors.inverseOnSurface,
         )
         HorizontalDivider(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalPadding(18.dp),
             thickness = 1.dp,
-            color = DmsTheme.colors.surface,
+            color = DmsTheme.colors.scrim,
         )
         DmsText(
             text = notice.content,
-            style = DmsTypography.Body2,
-            color = DmsTheme.colors.onTertiaryContainer,
+            style = DmsTypography.BodyM,
+            color = DmsTheme.colors.tertiaryContainer,
         )
     }
 }
