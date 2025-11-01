68ea4e3a95d8d889658ab265006946dc9f8763f1
uiel <parkuiery@gmail.com>
2025-11-01 20:50:03 +0900

    refactor :: core:common navtype 추가 및 serializeable 변경

--- core/common/src/commonMain/kotlin/team/aliens/dms/kmp/core/common/navtype/SerializableType.kt
+++ core/common/src/commonMain/kotlin/team/aliens/dms/kmp/core/common/navtype/SerializableType.kt
@@ -1,7 +1,9 @@
 package team.aliens.dms.kmp.core.common.navtype
 
-import androidx.core.bundle.Bundle
 import androidx.navigation.NavType
+import androidx.savedstate.SavedState
+import androidx.savedstate.read
+import androidx.savedstate.write
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
@@ -9,14 +11,20 @@ inline fun <reified T : Any?> serializableType(
     isNullableAllowed: Boolean = false,
     json: Json = Json,
 ) = object : NavType<T>(isNullableAllowed = isNullableAllowed) {
-    override fun get(bundle: Bundle, key: String) =
-        bundle.getString(key)?.let<String, T>(json::decodeFromString)
+
+    override fun get(bundle: SavedState, key: String): T? {
+        return bundle.read {
+            this.getString(key).let<String,T>(json::decodeFromString)
+        }
+    }
 
     override fun parseValue(value: String): T = json.decodeFromString(value)
 
     override fun serializeAsValue(value: T): String = Json.encodeToString(value)
 
-    override fun put(bundle: Bundle, key: String, value: T) {
-        bundle.putString(key, json.encodeToString(value))
+    override fun put(bundle: SavedState, key: String, value: T) {
+        return bundle.write {
+            this.putString(key, json.encodeToString(value))
+        }
     }
 }
