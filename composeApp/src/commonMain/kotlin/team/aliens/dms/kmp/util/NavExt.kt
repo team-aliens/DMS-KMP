945a90d03e68166d149c27975330c6c57f333a3a
uiel <parkuiery@gmail.com>
2025-11-01 20:47:30 +0900

    feat :: NavExt 구현

---
+++ composeApp/src/commonMain/kotlin/team/aliens/dms/kmp/util/NavExt.kt
@@ -0,0 +1,10 @@
+package team.aliens.dms.kmp.util
+
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+import kotlin.reflect.KClass
+
+fun NavDestination?.isRouteInHierarchy(route: KClass<*>) = this?.hierarchy?.any {
+    it.hasRoute(route)
+} ?: false
