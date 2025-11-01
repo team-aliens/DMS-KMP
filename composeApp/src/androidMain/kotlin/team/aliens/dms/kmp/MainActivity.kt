0fb194886e230a543b43c34c7ce63264454a2238
uiel <parkuiery@gmail.com>
2025-11-01 20:42:55 +0900

    feat :: MainActivity theme 적용

--- composeApp/src/androidMain/kotlin/team/aliens/dms/kmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/team/aliens/dms/kmp/MainActivity.kt
@@ -1,16 +1,26 @@
 package team.aliens.dms.kmp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import team.aliens.dms.kmp.core.designsystem.foundation.DmsTheme
 import team.aliens.dms.kmp.ui.DmsApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+        )
         setContent {
-            DmsApp()
+            DmsTheme {
+                DmsApp()
+            }
         }
     }
 }
