3955c2a8c9d020f8caa9c18f394325c9fbdb00df
uiel <parkuiery@gmail.com>
2025-11-07 20:34:22 +0900

    feat :: core:util NullableExt 추가

---
+++ core/util/src/commonMain/kotlin/team/aliens/dms/kmp/core/util/NullableExt.kt
@@ -0,0 +1,14 @@
+package team.aliens.dms.kmp.core.util
+
+inline fun <T : Any?> guardAll(vararg values: T?, onNull: () -> Nothing): List<T> {
+    return values.map { it ?: onNull() }
+}
+
+fun <A : Any, B : Any> guardAll(
+    a: A?, b: B?,
+): Pair<A, B>? = if (a != null && b != null) a to b else null
+
+fun <A : Any, B : Any, C : Any> guardAll(
+    a: A?, b: B?, c: C?,
+): Triple<A, B, C>? =
+    if (a != null && b != null && c != null) Triple(a, b, c) else null
