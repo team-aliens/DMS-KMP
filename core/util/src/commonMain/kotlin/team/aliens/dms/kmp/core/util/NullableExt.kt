efc53e66edf0fcafc3ef0d2d91b176a58334721d
uiel <parkuiery@gmail.com>
2025-11-07 20:42:54 +0900

    style :: ktlint

--- core/util/src/commonMain/kotlin/team/aliens/dms/kmp/core/util/NullableExt.kt
+++ core/util/src/commonMain/kotlin/team/aliens/dms/kmp/core/util/NullableExt.kt
@@ -5,10 +5,13 @@ inline fun <T : Any?> guardAll(vararg values: T?, onNull: () -> Nothing): List<T
 }
 
 fun <A : Any, B : Any> guardAll(
-    a: A?, b: B?,
+    a: A?,
+    b: B?,
 ): Pair<A, B>? = if (a != null && b != null) a to b else null
 
 fun <A : Any, B : Any, C : Any> guardAll(
-    a: A?, b: B?, c: C?,
+    a: A?,
+    b: B?,
+    c: C?,
 ): Triple<A, B, C>? =
     if (a != null && b != null && c != null) Triple(a, b, c) else null
