770e30521721587b70c052dae5b8d621d8878332
uiel <parkuiery@gmail.com>
2025-11-07 19:50:00 +0900

    refactor :: core:datastore witchContext 추가

--- core/datastore/src/commonMain/kotlin/team/aliens/dms/kmp/core/datastore/auth/AuthPreferencesDataSourceImpl.kt
+++ core/datastore/src/commonMain/kotlin/team/aliens/dms/kmp/core/datastore/auth/AuthPreferencesDataSourceImpl.kt
@@ -2,7 +2,10 @@ package team.aliens.dms.kmp.core.datastore.auth
 
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.withContext
 import kotlinx.datetime.LocalDateTime
 import team.aliens.dms.kmp.core.datastore.PreferencesDataStore
 import team.aliens.dms.kmp.core.datastore.auth.exception.CannotClearTokensException
@@ -17,43 +20,50 @@ internal class AuthPreferencesDataSourceImpl(
 ) : AuthPreferencesDataSource {
 
     override suspend fun loadTokens(): Result<TokenModel?> = kotlin.runCatching {
-        jwtDataStore.data.firstOrNull()?.let { preferences ->
-            val accessTokenValue = preferences[ACCESS_TOKEN] ?: return@let null
-            val accessTokenExpiration = preferences[ACCESS_TOKEN_EXPIRATION] ?: return@let null
-            val refreshTokenValue = preferences[REFRESH_TOKEN] ?: return@let null
-            val refreshTokenExpiration = preferences[REFRESH_TOKEN_EXPIRATION] ?: return@let null
+        withContext(Dispatchers.IO) {
+            jwtDataStore.data.firstOrNull()?.let { preferences ->
+                val accessTokenValue = preferences[ACCESS_TOKEN] ?: return@let null
+                val accessTokenExpiration = preferences[ACCESS_TOKEN_EXPIRATION] ?: return@let null
+                val refreshTokenValue = preferences[REFRESH_TOKEN] ?: return@let null
+                val refreshTokenExpiration =
+                    preferences[REFRESH_TOKEN_EXPIRATION] ?: return@let null
 
-            TokenModel(
-                accessToken = AccessToken(
-                    value = accessTokenValue,
-                    expiration = LocalDateTime.parse(accessTokenExpiration),
-                ),
-                refreshToken = RefreshToken(
-                    value = refreshTokenValue,
-                    expiration = LocalDateTime.parse(refreshTokenExpiration),
-                ),
-            )
+                TokenModel(
+                    accessToken = AccessToken(
+                        value = accessTokenValue,
+                        expiration = LocalDateTime.parse(accessTokenExpiration),
+                    ),
+                    refreshToken = RefreshToken(
+                        value = refreshTokenValue,
+                        expiration = LocalDateTime.parse(refreshTokenExpiration),
+                    ),
+                )
+            }
         }
     }
 
     override suspend fun storeTokens(token: TokenModel): Result<Unit> = runCatching {
-        transform(onFailure = { throw CannotStoreTokensException() }) {
-            jwtDataStore.edit { preferences ->
-                val accessToken = token.accessToken
-                val refreshToken = token.refreshToken
-                preferences[ACCESS_TOKEN] = accessToken.value
-                preferences[ACCESS_TOKEN_EXPIRATION] = accessToken.expiration.toString()
-                preferences[REFRESH_TOKEN] = refreshToken.value
-                preferences[REFRESH_TOKEN_EXPIRATION] = refreshToken.expiration.toString()
+        withContext(Dispatchers.IO) {
+            transform(onFailure = { throw CannotStoreTokensException() }) {
+                jwtDataStore.edit { preferences ->
+                    val accessToken = token.accessToken
+                    val refreshToken = token.refreshToken
+                    preferences[ACCESS_TOKEN] = accessToken.value
+                    preferences[ACCESS_TOKEN_EXPIRATION] = accessToken.expiration.toString()
+                    preferences[REFRESH_TOKEN] = refreshToken.value
+                    preferences[REFRESH_TOKEN_EXPIRATION] = refreshToken.expiration.toString()
+                }
             }
         }
     }
 
     override suspend fun clearTokens(): Result<Unit> = kotlin.runCatching {
-        transform(
-            onFailure = { throw CannotClearTokensException() },
-        ) {
-            jwtDataStore.edit { preferences -> preferences.clear() }
+        withContext(Dispatchers.IO) {
+            transform(
+                onFailure = { throw CannotClearTokensException() },
+            ) {
+                jwtDataStore.edit { preferences -> preferences.clear() }
+            }
         }
     }
 
