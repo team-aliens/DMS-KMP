0569aedc8f23504bff65147546be4f7dffb3dae0
uiel <parkuiery@gmail.com>
2025-11-09 21:30:28 +0900

    refactor :: core:designsystem Clickable singleClick 추가

--- core/design-system/src/commonMain/kotlin/team/aliens/dms/kmp/core/designsystem/util/Clickable.kt
+++ core/design-system/src/commonMain/kotlin/team/aliens/dms/kmp/core/designsystem/util/Clickable.kt
@@ -9,9 +9,14 @@ import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 internal const val DURATION_MILLIS = 100
 internal const val DEFAULT_PRESS_DEPTH = 0.98f
@@ -22,13 +27,15 @@ internal const val DEFAULT_DISABLED_MILLIS = 300L
 fun Modifier.clickable(
     enabled: Boolean = true,
     pressDepth: Float = DEFAULT_PRESS_DEPTH,
-    indication: Indication = ripple(),
+    indication: Indication? = ripple(),
+    isSingleClick: Boolean = true,
     onPressed: ((pressed: Boolean) -> Unit)? = null,
     onClick: () -> Unit,
     disabledMillis: Long = DEFAULT_DISABLED_MILLIS,
 ): Modifier {
     val interactionSource = remember { MutableInteractionSource() }
     val isPressed by interactionSource.collectIsPressedAsState()
+    var isClickable by remember { mutableStateOf(true) }
     val scale by animateFloatAsState(
         targetValue = if (isPressed) {
             pressDepth
@@ -37,6 +44,7 @@ fun Modifier.clickable(
         },
 //        animationSpec = tween(delayMillis = DURATION_MILLIS),
     )
+    val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(isPressed) {
         onPressed?.invoke(isPressed)
@@ -44,11 +52,21 @@ fun Modifier.clickable(
 
     return this
         .clickable(
-            onClick = onClick,
-            enabled = enabled,
+            enabled = enabled && isClickable,
             indication = indication,
             interactionSource = interactionSource,
-        )
+        ) {
+            if (isSingleClick) {
+                isClickable = false
+                onClick()
+                coroutineScope.launch {
+                    delay(disabledMillis)
+                    isClickable = true
+                }
+            } else {
+                onClick()
+            }
+        }
         .graphicsLayer(
             scaleX = scale,
             scaleY = scale,
