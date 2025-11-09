8b9f0d3bba9254d6ffa6b22adcd6d084561db3d5
uiel <parkuiery@gmail.com>
2025-11-09 20:37:21 +0900

    refactor :: core:designsystem clickabe 변경

--- core/design-system/src/commonMain/kotlin/team/aliens/dms/kmp/core/designsystem/util/Clickable.kt
+++ core/design-system/src/commonMain/kotlin/team/aliens/dms/kmp/core/designsystem/util/Clickable.kt
@@ -1,25 +1,19 @@
 package team.aliens.dms.kmp.core.designsystem.util
 
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.indication
+import androidx.compose.foundation.Indication
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableLongStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.changedToDown
-import androidx.compose.ui.input.pointer.changedToUp
-import androidx.compose.ui.input.pointer.pointerInput
-import team.aliens.dms.kmp.core.util.timeNow
 
-internal const val DURATION_MILLIS = 200
+internal const val DURATION_MILLIS = 100
 internal const val DEFAULT_PRESS_DEPTH = 0.98f
 internal const val MIN_PRESS_DEPTH = 1f
 internal const val DEFAULT_DISABLED_MILLIS = 300L
@@ -28,74 +22,35 @@ internal const val DEFAULT_DISABLED_MILLIS = 300L
 fun Modifier.clickable(
     enabled: Boolean = true,
     pressDepth: Float = DEFAULT_PRESS_DEPTH,
+    indication: Indication = ripple(),
     onPressed: ((pressed: Boolean) -> Unit)? = null,
     onClick: () -> Unit,
     disabledMillis: Long = DEFAULT_DISABLED_MILLIS,
 ): Modifier {
-    var pressed by remember { mutableStateOf(false) }
-    var lastClick by remember { mutableLongStateOf(0L) }
     val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
     val scale by animateFloatAsState(
-        targetValue = if (pressed) {
+        targetValue = if (isPressed) {
             pressDepth
         } else {
             1f
         },
-        animationSpec = tween(delayMillis = DURATION_MILLIS),
+//        animationSpec = tween(delayMillis = DURATION_MILLIS),
     )
 
-    return this then Modifier
+    LaunchedEffect(isPressed) {
+        onPressed?.invoke(isPressed)
+    }
+
+    return this
+        .clickable(
+            onClick = onClick,
+            enabled = enabled,
+            indication = indication,
+            interactionSource = interactionSource,
+        )
         .graphicsLayer(
             scaleX = scale,
             scaleY = scale,
         )
-        .indication(
-            interactionSource = interactionSource,
-            indication = ripple(
-                bounded = true,
-                color = Color.Red,
-            ),
-        )
-        .pointerInput(Unit) {
-            if (enabled) {
-                awaitPointerEventScope {
-                    while (true) {
-                        val event = awaitPointerEvent()
-                        event.changes.forEach { change ->
-                            when {
-                                change.changedToDown() -> {
-                                    pressed = true
-                                    onPressed?.invoke(true)
-                                }
-
-                                change.changedToUp() || change.isConsumed -> {
-                                    // TODO: 클릭 처리 방식 변경 필요
-                                    /*pressed = false
-                                    onPressed?.invoke(false)
-                                    if (change.changedToUp() && timeNow.toNanosecondOfDay() - lastClick >= disabledMillis) {
-                                        lastClick = timeNow.toNanosecondOfDay()
-                                        onClick()
-                                    }*/
-                                    val isInside =
-                                        change.position.x in 0f..size.width.toFloat() && change.position.y in 0f..size.height.toFloat()
-
-                                    pressed = false
-                                    onPressed?.invoke(false)
-
-                                    // 클릭 범위 내부에서만 onClick 실행
-                                    if (
-                                        isInside &&
-                                        change.changedToUp() &&
-                                        timeNow.toNanosecondOfDay() - lastClick >= disabledMillis
-                                    ) {
-                                        lastClick = timeNow.toNanosecondOfDay()
-                                        onClick()
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
 }
