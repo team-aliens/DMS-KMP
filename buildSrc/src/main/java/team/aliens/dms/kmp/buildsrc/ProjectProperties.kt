435116626d996912921b8b527297bdda05c09fa6
uiel <parkuiery@gmail.com>
2025-11-22 17:57:49 +0900

    chore :: version update

--- buildSrc/src/main/java/team/aliens/dms/kmp/buildsrc/ProjectProperties.kt
+++ buildSrc/src/main/java/team/aliens/dms/kmp/buildsrc/ProjectProperties.kt
@@ -1,8 +1,8 @@
 package team.aliens.dms.kmp.buildsrc
 
 object ProjectProperties {
-    const val VERSION_CODE = 2
-    const val VERSION_NAME = "0.0.1"
+    const val VERSION_CODE = 3
+    const val VERSION_NAME = "0.0.2"
     const val MIN_SDK = 28
     const val TARGET_SDK = 35
     const val COMPILE_SDK = 35
