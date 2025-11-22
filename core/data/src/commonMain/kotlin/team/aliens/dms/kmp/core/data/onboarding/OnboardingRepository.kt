963f6aa164bb2c7d16e3f996fabfe2575e169cb6
uiel <parkuiery@gmail.com>
2025-11-22 17:59:11 +0900

    feat :: core:data OnboardingRepository 추가

---
+++ core/data/src/commonMain/kotlin/team/aliens/dms/kmp/core/data/onboarding/OnboardingRepository.kt
@@ -0,0 +1,6 @@
+package team.aliens.dms.kmp.core.data.onboarding
+
+interface OnboardingRepository {
+    suspend fun getOnboardingCompleted(): Result<Boolean>
+    suspend fun setOnboardingCompleted(isCompleted: Boolean): Result<Unit>
+}
