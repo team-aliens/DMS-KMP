921b047ad7ed405cbd0b1cc0e631cab56ab0ff58
uiel <parkuiery@gmail.com>
2026-01-09 00:15:58 +0900

    refactor :: core:common CountDownTimer stateFlow 추가

--- core/common/src/jvmMain/kotlin/team/aliens/dms/kmp/core/common/timer/CountDownTimer.jvm.kt
+++ core/common/src/jvmMain/kotlin/team/aliens/dms/kmp/core/common/timer/CountDownTimer.jvm.kt
@@ -1,5 +1,6 @@
 package team.aliens.dms.kmp.core.common.timer
 
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
@@ -8,7 +9,7 @@ actual class CountDownTimer actual constructor(
     private val intervalMillis: Long,
 ) {
     actual var listener: CountDownTimerListener? = null
-    actual val isRunning: StateFlow<Boolean> = false
+    actual val isRunning: StateFlow<Boolean> = MutableStateFlow(false)
 
     actual fun start() {}
 
