89544ecf6f4217495a254a723b4631e033bac452
유섭 <yoonbk2001027@gmail.com>
2026-01-27 13:40:46 +0900

    refactor :: 알람 상태 model 초기화 값 적용

--- core/model/src/commonMain/kotlin/team/aliens/dms/kmp/core/model/notification/NotificationTopicStatusModel.kt
+++ core/model/src/commonMain/kotlin/team/aliens/dms/kmp/core/model/notification/NotificationTopicStatusModel.kt
@@ -4,16 +4,16 @@ import team.aliens.dms.kmp.core.model.type.NotificationGroupType
 import team.aliens.dms.kmp.core.model.type.NotificationType
 
 data class NotificationTopicStatusModel(
-    val topicGroups: List<TopicGroup>,
+    val topicGroups: List<TopicGroup> = emptyList(),
 ) {
     data class TopicGroup(
-        val topicGroup: NotificationGroupType,
-        val groupName: String,
-        val topicSubscriptions: List<TopicSubscription>,
+        val topicGroup: NotificationGroupType = NotificationGroupType.STUDY_ROOM,
+        val groupName: String = "",
+        val topicSubscriptions: List<TopicSubscription> = emptyList(),
     ) {
         data class TopicSubscription(
-            val topic: NotificationType,
-            val isSubscribed: Boolean,
+            val topic: NotificationType = NotificationType.STUDY_ROOM_APPLY,
+            val isSubscribed: Boolean = false,
         )
     }
 }
