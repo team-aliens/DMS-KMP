b3c5c7e54aab2fd8723642b7cd56d2da76540a7f
유섭 <yoonbk2001027@gmail.com>
2026-02-09 12:05:12 +0900

    feat :: android firebase sdk 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,6 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.googleServices) apply false
+    alias(libs.plugins.firebaseCrashlytics) apply false
+    alias(libs.plugins.firebasePerformance) apply false
 }
