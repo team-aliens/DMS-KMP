dbef976fd49dcf726b6da8efeb749eee9c9215e6
ashxom <anseha090115@naver.com>
2026-04-24 16:20:10 +0900

    feat :: LateStudyRepository 인터페이스 추가

--- core/data/src/commonMain/kotlin/team/aliens/dms/kmp/core/data/latestudy/repository/LateStudyRepository.kt
+++ core/data/src/commonMain/kotlin/team/aliens/dms/kmp/core/data/latestudy/repository/LateStudyRepository.kt
@@ -1,4 +1,16 @@
 package team.aliens.dms.kmp.core.data.latestudy.repository
 
-class LateStudyRepository {
+import team.aliens.dms.kmp.core.network.latestudy.model.request.SubmitLateStudyRequest
+import team.aliens.dms.kmp.core.network.latestudy.model.response.FetchStudyTypesResponse
+import team.aliens.dms.kmp.core.network.latestudy.model.response.FetchTeachersResponse
+import team.aliens.dms.kmp.core.network.latestudy.model.response.StudyApplicationStatusResponse
+
+interface LateStudyRepository {
+    suspend fun fetchStudyTypes(): FetchStudyTypesResponse
+
+    suspend fun fetchTeachers(): FetchTeachersResponse
+
+    suspend fun fetchMyStudyApplicationStatus(): StudyApplicationStatusResponse
+
+    suspend fun submitLateStudy(request: SubmitLateStudyRequest)
 }
