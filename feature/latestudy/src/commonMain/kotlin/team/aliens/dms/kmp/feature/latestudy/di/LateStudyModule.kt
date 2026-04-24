58be363e73f23fa1d4a573c04787f84edf2aecb0
ashxom <anseha090115@naver.com>
2026-04-24 14:54:04 +0900

    feat :: 새벽 자습 Route 및 화면 연결 추가

--- feature/latestudy/src/commonMain/kotlin/team/aliens/dms/kmp/feature/latestudy/di/LateStudyModule.kt
+++ feature/latestudy/src/commonMain/kotlin/team/aliens/dms/kmp/feature/latestudy/di/LateStudyModule.kt
@@ -1,4 +1,16 @@
 package team.aliens.dms.kmp.feature.latestudy.di
 
-class LateStudyModule {
+import androidx.compose.runtime.Composable
+import team.aliens.dms.kmp.core.designsystem.snackbar.DmsSnackBarType
+import team.aliens.dms.kmp.feature.latestudy.ui.LateStudyScreen
+
+@Composable
+fun LateStudyRoute(
+    onBack: () -> Unit,
+    onShowSnackBar: (DmsSnackBarType, String) -> Unit,
+) {
+    LateStudyScreen(
+        onBack = onBack,
+        onShowSnackBar = onShowSnackBar,
+    )
 }
