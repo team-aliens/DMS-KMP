50107bf352bff9a77fbe33d57ba37c904cf52499
ashxom <anseha090115@naver.com>
2026-05-10 12:11:39 +0900

    fix :: 새벽 자습 상태 조회 의존성 주입 추가

--- feature/application/src/commonMain/kotlin/team/aliens/dms/kmp/feature/application/di/ApplicationModule.kt
+++ feature/application/src/commonMain/kotlin/team/aliens/dms/kmp/feature/application/di/ApplicationModule.kt
@@ -1,9 +1,15 @@
 package team.aliens.dms.kmp.feature.application.di
 
-import org.koin.core.module.dsl.viewModelOf
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 import team.aliens.dms.kmp.feature.application.viewmodel.ApplicationViewModel
 
 val applicationModule = module {
-    viewModelOf(::ApplicationViewModel)
+    viewModel {
+        ApplicationViewModel(
+            getRemainUseCase = get(),
+            getAllVotesUseCase = get(),
+            lateStudyRepository = get(),
+        )
+    }
 }
