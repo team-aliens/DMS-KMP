dfcc2e4594222c50625557f57f617fbcf1495406
ashxom <anseha090115@naver.com>
2026-05-10 12:16:08 +0900

    fix :: ApplicationViewModel 의존성 오류 수정

--- feature/application/src/commonMain/kotlin/team/aliens/dms/kmp/feature/application/di/ApplicationModule.kt
+++ feature/application/src/commonMain/kotlin/team/aliens/dms/kmp/feature/application/di/ApplicationModule.kt
@@ -1,15 +1,9 @@
 package team.aliens.dms.kmp.feature.application.di
 
-import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 import team.aliens.dms.kmp.feature.application.viewmodel.ApplicationViewModel
 
 val applicationModule = module {
-    viewModel {
-        ApplicationViewModel(
-            getRemainUseCase = get(),
-            getAllVotesUseCase = get(),
-            lateStudyRepository = get(),
-        )
-    }
+    viewModelOf(::ApplicationViewModel)
 }
