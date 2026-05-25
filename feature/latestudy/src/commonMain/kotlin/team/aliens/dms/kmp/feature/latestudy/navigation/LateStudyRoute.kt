4e898ae8df18a09429a5bf89c2c4912fdf36a882
ashxom <anseha090115@naver.com>
2026-05-25 11:13:29 +0900

    feat :: 새벽 자습 라우트 및 네비게이션 추가

--- feature/latestudy/src/commonMain/kotlin/team/aliens/dms/kmp/feature/latestudy/navigation/LateStudyRoute.kt
+++ feature/latestudy/src/commonMain/kotlin/team/aliens/dms/kmp/feature/latestudy/navigation/LateStudyRoute.kt
@@ -1,16 +1,25 @@
 package team.aliens.dms.kmp.feature.latestudy.navigation
 
-import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import kotlinx.serialization.Serializable
 import team.aliens.dms.kmp.core.designsystem.snackbar.DmsSnackBarType
 import team.aliens.dms.kmp.feature.latestudy.ui.LateStudyScreen
 
-@Composable
-fun LateStudyRoute(
-    onBack: () -> Unit,
+@Serializable
+data object LateStudyRoute
+
+fun NavController.navigateToLateStudy() = navigate(LateStudyRoute)
+
+fun NavGraphBuilder.lateStudy(
+    onNavigateBack: () -> Unit,
     onShowSnackBar: (DmsSnackBarType, String) -> Unit,
 ) {
-    LateStudyScreen(
-        onBack = onBack,
-        onShowSnackBar = onShowSnackBar,
-    )
+    composable<LateStudyRoute> {
+        LateStudyScreen(
+            onBack = onNavigateBack,
+            onShowSnackBar = onShowSnackBar,
+        )
+    }
 }
